70549f6166c333339f3d7c63be54fc422e8d8b48
holishing <holishingtest@gmail.com>
2017-07-30 06:22:14 +0800

    let our config just for ref.

--- web/config-sample.php
+++ web/config-sample.php
@@ -1,12 +1,16 @@
 <?php
-define('SITE_NAME','');
+
+define('SITE_NAME','夢之大地');
 //這個為站台名稱
-define('SITE_DOMAIN','');
-//站台網址 或ip
-define('WEBSITE_DOMAIN','');
+
+define('SITE_DOMAIN','ccns.cc');
+//站台域名 或 ip
+
+define('WEBSITE_DOMAIN','bbs.ccns.cc');
 //好讀版的網址
 
-define('DB_PATH','');
+define('DB_PATH','../count.db');
 //SQLite3 file的位置
-include('uaocode.php');//這行請不用動他
+
+include('uaocode.php'); //這行請不用動他
 ?>
