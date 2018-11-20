4329dda9e5c12dc6ee927d697b365a80dcb24df0
holishing <holishingtest@gmail.com>
2018-11-21 00:32:05 +0800

    v0.95.3: update release number

--- include/config.h
+++ include/config.h
@@ -35,7 +35,7 @@
 #endif
 
 #define BBSVERNAME      "DreamBBS"                  /* 版本名稱 */
-#define BBSVERSION      "Rev.Beta 4.2"              /* 版本編號 */
+#define BBSVERSION      "Rev.Beta 4.3"              /* 版本編號 */
 
 #ifndef SYSOPNICK
 #define SYSOPNICK       "夢之精靈"                  /* SYSOP 暱稱 */
