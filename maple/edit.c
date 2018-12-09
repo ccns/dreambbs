ccns/dreambbs

commit 241b1328e93df7bad52c5df65a2a9b6fe357a8a6

Adjust the spaces around the commas.

maple/edit.c
@@ -295,7 +295,7 @@ n2ansi(
 
 
 /* ----------------------------------------------------- */
-/* delete_line deletes 'line' from the list,		 */
+/* delete_line deletes 'line' from the list, 		 */
 /* and maintains the vx_ini pointers.			 */
 /* ----------------------------------------------------- */
 
@@ -932,7 +932,7 @@ quote_line(
             strstr(str, ") 提到:\n"))
             return 0;
     }
-    return (*str != '\n' && (strncmp(str,"\033[m\n",4)));
+    return (*str != '\n' && (strncmp(str, "\033[m\n", 4)));
 }
 
 /* ----------------------------------------------------- */
@@ -1221,7 +1221,7 @@ ve_header(
     {
 
         /* Thor.980613: 如果在轉錄文章, 呼叫 ve_header, 由於不是在被轉的版, 因此屬性不對,
-                                        有可能使秘密版出現 , BRD_ANONYMOUS 亦同*/
+                                        有可能使秘密版出現, BRD_ANONYMOUS 亦同*/
 
         if (!(bbstate & BRD_NOSTAT))/* 看板不納入統計 */
         {
@@ -1280,7 +1280,7 @@ ve_header(
                 HAS_PERM(PERM_DENYNICK) ? cutmp->username : cuser.username
 #endif
 
-                ,curredit & EDIT_OUTGO ? str_post1 : str_post2, currboard);
+               , curredit & EDIT_OUTGO ? str_post1 : str_post2, currboard);
         }
     }
     fprintf(fp, "標題: %s\n時間: %s\n", title, ctime(&now));
@@ -1291,19 +1291,19 @@ ve_show_sign(
     char *fpath)
 {
     char buf[256];
-    int i,j;
+    int i, j;
     FILE *fp;
-    fp = fopen(fpath,"r");
+    fp = fopen(fpath, "r");
     clear();
     if (fp)
     {
         for (j=1;j<=3;j++)
         {
-            prints("\033[36m【簽名檔.%d】\033[m\n",j);
+            prints("\033[36m【簽名檔.%d】\033[m\n", j);
             for (i=0;i<MAXSIGLINES;i++)
             {
                 if (fgets(buf, 256, fp))
-                    prints("%s",buf);
+                    prints("%s", buf);
                 else
                     prints("\n");
             }
@@ -1320,7 +1320,7 @@ ve_select_sign(
     FILE *fd;
     char msg[] = "選擇簽名檔 (1/2/3, 0=不加)[0]：";
     char buf[256];
-    int ans,op,len,used;
+    int ans, op, len, used;
 
     msg[27] = op = cuser.signature + '0';
     usr_fpath(buf, cuser.userid, FN_SIGN);
@@ -1345,7 +1345,7 @@ ve_select_sign(
             op = (op - '1') * MAXSIGLINES;
             for (i=0;i<op;i++)
                 fgets(buf, 256, fd);
-            fprintf(fp,"--");
+            fprintf(fp, "--");
 
             len = 0;
             used = 0;
@@ -1353,20 +1353,20 @@ ve_select_sign(
             {
                 if (fgets(buf, 256, fd))
                 {
-                    fprintf(fp,"\n");
+                    fprintf(fp, "\n");
                     len = strlen(buf);
                     if (len > 0)
                     {
                         used = 1;
                         buf[len - 1] = '\0';
-                        fprintf(fp,"%s",buf);
+                        fprintf(fp, "%s", buf);
                     }
                 }
             }
             if (used)
-                fprintf(fp,"\033[m\n");
+                fprintf(fp, "\033[m\n");
             else
-                fprintf(fp,"\n");
+                fprintf(fp, "\n");
             fclose(fd);
         }
     }
@@ -1381,16 +1381,16 @@ ve_filer(
     int ans = 0;
     FILE *fp=NULL;
     textline *p, *v;
-    char buf[80], *msg , re;
+    char buf[80], *msg, re;
 
 #ifdef  HAVE_INPUT_TOOLS
-    char *menu1[] =  {"SE","Save     存檔","Abort    放棄","Title    改標題","Edit     繼續編輯","Input    符號輸入工具","Read     讀取暫存檔","Write    寫入暫存檔","Delete   刪除暫存檔" , "Quit     離開選單" , NULL};
-    char *menu2[] = {"SE","Save     存檔","Local    存為站內檔","Abort    放棄","Title    改標題","Edit     繼續編輯","Input    符號輸入工具","Read     讀取暫存檔","Write    寫入暫存檔","Delete   刪除暫存檔" , "Quit     離開選單" , NULL};
-    char *menu3[] = {"LE","Local    存為站內檔","Save     存檔","Abort    放棄","Title    改標題","Edit     繼續編輯","Input    符號輸入工具","Read     讀取暫存檔","Write    寫入暫存檔","Delete   刪除暫存檔" , "Quit     離開選單" , NULL};
+    char *menu1[] =  {"SE", "Save     存檔", "Abort    放棄", "Title    改標題", "Edit     繼續編輯", "Input    符號輸入工具", "Read     讀取暫存檔", "Write    寫入暫存檔", "Delete   刪除暫存檔", "Quit     離開選單", NULL};
+    char *menu2[] = {"SE", "Save     存檔", "Local    存為站內檔", "Abort    放棄", "Title    改標題", "Edit     繼續編輯", "Input    符號輸入工具", "Read     讀取暫存檔", "Write    寫入暫存檔", "Delete   刪除暫存檔", "Quit     離開選單", NULL};
+    char *menu3[] = {"LE", "Local    存為站內檔", "Save     存檔", "Abort    放棄", "Title    改標題", "Edit     繼續編輯", "Input    符號輸入工具", "Read     讀取暫存檔", "Write    寫入暫存檔", "Delete   刪除暫存檔", "Quit     離開選單", NULL};
 #else
-    char *menu1[] =  {"SE","Save     存檔","Abort    放棄","Title    改標題","Edit     繼續編輯","Read     讀取暫存檔","Write    寫入暫存檔","Delete   刪除暫存檔" , "Quit     離開選單" , NULL};
-    char *menu2[] = {"SE","Save     存檔","Local    存為站內檔","Abort    放棄","Title    改標題","Edit     繼續編輯","Read     讀取暫存檔","Write    寫入暫存檔","Delete   刪除暫存檔" , "Quit     離開選單" , NULL};
-    char *menu3[] = {"LE","Local    存為站內檔","Save     存檔","Abort    放棄","Title    改標題","Edit     繼續編輯","Read     讀取暫存檔","Write    寫入暫存檔","Delete   刪除暫存檔" , "Quit     離開選單" , NULL};
+    char *menu1[] =  {"SE", "Save     存檔", "Abort    放棄", "Title    改標題", "Edit     繼續編輯", "Read     讀取暫存檔", "Write    寫入暫存檔", "Delete   刪除暫存檔", "Quit     離開選單", NULL};
+    char *menu2[] = {"SE", "Save     存檔", "Local    存為站內檔", "Abort    放棄", "Title    改標題", "Edit     繼續編輯", "Read     讀取暫存檔", "Write    寫入暫存檔", "Delete   刪除暫存檔", "Quit     離開選單", NULL};
+    char *menu3[] = {"LE", "Local    存為站內檔", "Save     存檔", "Abort    放棄", "Title    改標題", "Edit     繼續編輯", "Read     讀取暫存檔", "Write    寫入暫存檔", "Delete   刪除暫存檔", "Quit     離開選單", NULL};
 #endif
 
 
@@ -1416,11 +1416,11 @@ ve_filer(
 //  else
 //  {
         if (bbsmode != M_POST)
-            re = popupmenu_ans2(menu1,"存檔選項",4,20);
+            re = popupmenu_ans2(menu1, "存檔選項", 4, 20);
         else if (curredit & EDIT_OUTGO)
-            re = popupmenu_ans2(menu2,"存檔選項",4,20);
+            re = popupmenu_ans2(menu2, "存檔選項", 4, 20);
         else
-            re = popupmenu_ans2(menu3,"存檔選項",4,20);
+            re = popupmenu_ans2(menu3, "存檔選項", 4, 20);
 
 //  }
 
@@ -1560,7 +1560,7 @@ ve_filer(
             if (ve_op)
             {
                 fprintf(fp, ORIGIN_TAG,
-                    /*str_site, MYHOSTNAME,*/ ((cuser.ufo & UFO_HIDDEN)&&(str_cmp(cuser.userid,"guest"))) ? HIDDEN_SRC : fromhost);
+                    /*str_site, MYHOSTNAME, */ ((cuser.ufo & UFO_HIDDEN)&&(str_cmp(cuser.userid, "guest"))) ? HIDDEN_SRC : fromhost);
 
                 if ((bbstate & BRD_LOGEMAIL) && !(bbsmode == M_SMAIL))
                     fprintf(fp, EMAIL_TAG, cuser.email);
@@ -1617,13 +1617,13 @@ static int
 select_title(
     char *title)
 {
-    char *objs[] = {"[公告]","[新聞]","[閒聊]","[文件]","[問題]","[測試]"};
+    char *objs[] = {"[公告]", "[新聞]", "[閒聊]", "[文件]", "[問題]", "[測試]"};
     int select;
     outs("\n\n1.【公告】2.【新聞】3.【閒聊】4.【文件】5.【問題】6.【測試】7.【其他】\n");
     select = vans("請選擇文章類別或按 Enter 跳過：") - '1';
     if (select >=0 && select <=5)
     {
-        sprintf(title,"%s",objs[select]);
+        sprintf(title, "%s", objs[select]);
         return 1;
     }
     else
@@ -1810,7 +1810,7 @@ vedit(
             move(b_lines, 0);
             clrtoeol();
 #ifdef EVERY_BIFF
-            prints("%s\033[0;31;47m  (Ctrl-Z)\033[30m 操作說明  \033[31m(^w,^x)\033[30m 檔案處理 ║%s│%s║%5d:%3d  \033[m",
+            prints("%s\033[0;31;47m  (Ctrl-Z)\033[30m 操作說明  \033[31m(^w, ^x)\033[30m 檔案處理 ║%s│%s║%5d:%3d  \033[m",
                 mode & VE_BIFF ? "\033[1;41;37;5m  郵差來了  ": mode & VE_BIFFN ? "\033[1;41;37;5m  訊差來了  ":"\033[0;34;46m  編輯文章  ",
                 mode & VE_INSERT ? "插入" : "取代",
                 mode & VE_ANSI ? "ANSI" : "一般",
@@ -1818,7 +1818,7 @@ vedit(
                 /* Thor.980805: UFO_BIFF everywhere */
 #else
 
-            prints("\033[34;46m  編輯文章  \033[31;47m  (Ctrl-Z)\033[30m 操作說明  \033[31m(^w,^x)\033[30m 檔案處理 ║%s│%s║%5d:%3d  \033[m",
+            prints("\033[34;46m  編輯文章  \033[31;47m  (Ctrl-Z)\033[30m 操作說明  \033[31m(^w, ^x)\033[30m 檔案處理 ║%s│%s║%5d:%3d  \033[m",
                 mode & VE_INSERT ? "插入" : "取代",
                 mode & VE_ANSI ? "ANSI" : "一般",
                 ve_lno, 1 + (mode & VE_ANSI ? pos : col));
@@ -2161,7 +2161,7 @@ ve_key:
                         NULL
                     };
 
-                    switch (cc = popupmenu_ans2(menu,"控制碼選擇", 7, 20))
+                    switch (cc = popupmenu_ans2(menu, "控制碼選擇", 7, 20))
                     {
                     case 'i':
                         ve_char(KEY_ESC);

so/bank.c
@@ -64,9 +64,9 @@ int point1_money(void)
 #endif
     vs_bar("夢幣轉換");
 
-    move(2,0);
+    move(2, 0);
     prints("你的身上有 %9d 夢幣\n\n           %9d 優良點數"
-                    ,acct.money, acct.point1);
+                   , acct.money, acct.point1);
     if (acct.point1 < 1)
     {
         pmsg2("優良點數不足");
@@ -118,7 +118,7 @@ TransferAccount(void)
     time_t now;
     HDR xhdr;
     FILE *fp;
-    char folder[128],date[9],fpath[128];
+    char folder[128], date[9], fpath[128];
     char userid[13];
     char str[128];
     int selfmoney, pay;
@@ -135,21 +135,21 @@ TransferAccount(void)
     prints("\033[1;33m轉帳相關規定： \033[36m１. 一次最少要給 100 夢幣(稅前)。\n"
         "                       ２. 收手續費 10 %%。\033[m");
 
-    if (acct_get("要匯給誰：",&acct)<1)
+    if (acct_get("要匯給誰：", &acct)<1)
         return 0;
     if (acct.userno == cuser.userno)
     {
         pmsg2("不能跟自己交易啦！");
         return 0;
     }
-    strcpy(userid,acct.userid);
+    strcpy(userid, acct.userid);
 
 #ifdef M3_USE_PFTERM
-    clrregion(1,21);
+    clrregion(1, 21);
 #else
-    clearange(1,21);
+    clearange(1, 21);
 #endif
-    move(3,0);
+    move(3, 0);
 
     if (acct_load(&selfacct, cuser.userid) >= 0)
         selfmoney = selfacct.money;
@@ -159,10 +159,10 @@ TransferAccount(void)
         return 0;
     }
 
-    prints("你自己的身上還有 %9d 夢幣。\n",selfacct.money);
-    prints("\n%-12s則有 %9d 夢幣。",userid, acct.money);
+    prints("你自己的身上還有 %9d 夢幣。\n", selfacct.money);
+    prints("\n%-12s則有 %9d 夢幣。", userid, acct.money);
 
-    if (!vget(7,0,"你要匯款多少夢幣：",buf,10,DOECHO))
+    if (!vget(7, 0, "你要匯款多少夢幣：", buf, 10, DOECHO))
         return 0;
 
     temp = ((int)atoi(buf) + acct.money);
@@ -185,10 +185,10 @@ TransferAccount(void)
 
     pay = (int)(atoi(buf)*1.1);
 
-    move(9,0);
+    move(9, 0);
     prints("欲轉 %d 元夢幣(稅前)，實際支付 %d 夢幣(稅後)", (int)atoi(buf), pay);
 
-    move(11,0);
+    move(11, 0);
     clrtobot();
 
     if (!vget(b_lines, 0, "匯款理由：", str, 60, DOECHO))
@@ -201,17 +201,17 @@ TransferAccount(void)
         str_stamp(date, &now);
 
         usr_fpath(folder, userid, FN_DIR);
-        fp = fdopen(hdr_stamp(folder, 0, &xhdr, fpath),"w");
+        fp = fdopen(hdr_stamp(folder, 0, &xhdr, fpath), "w");
         strcpy(xhdr.owner, cuser.userid);
         strcpy(xhdr.nick, cuser.username);
         sprintf(xhdr.title, "匯款通知");
         strcpy(xhdr.date, date);
 
-        fprintf(fp,"作者: %s (%s)\n",cuser.userid,cuser.username);
-        fprintf(fp,"標題: 匯款通知\n");
-        fprintf(fp,"時間: %s\n",date);
-        fprintf(fp,"\n\n%s 送給你 \033[1;31m%d\033[m 夢幣，請笑納~\n", cuser.userid, (int)atoi(buf));
-        fprintf(fp,"\n\n匯款理由：%s\n",str);
+        fprintf(fp, "作者: %s (%s)\n", cuser.userid, cuser.username);
+        fprintf(fp, "標題: 匯款通知\n");
+        fprintf(fp, "時間: %s\n", date);
+        fprintf(fp, "\n\n%s 送給你 \033[1;31m%d\033[m 夢幣，請笑納~\n", cuser.userid, (int)atoi(buf));
+        fprintf(fp, "\n\n匯款理由：%s\n", str);
         fclose(fp);
         rec_add(folder, &xhdr, sizeof(HDR));
 
@@ -276,7 +276,7 @@ money_back(void)
     }
     {
         clrtobot();
-        usr_fpath(fpath,acct.userid,FN_MONEY);
+        usr_fpath(fpath, acct.userid, FN_MONEY);
 
         //讀取MONEY
         if ((fd = open(fpath, O_RDONLY)) < 0)
@@ -358,20 +358,20 @@ int bank_main(void)
 
     clear();
 
-    move(0,0);
+    move(0, 0);
     prints("\033[1;33;42m                            " BOARDNAME "    銀行                                   \033[m\n\n");
-    move(10,0);
+    move(10, 0);
     prints("  您的帳號資訊如下    １. 用優良點數換取夢幣\n\n");
     prints("                      ２. 用夢幣換取優良點數\n\n");
     prints("                      ３. 投資\n\n");
     prints("                      ４. 匯款給其他人\n");
     prints("\n");
 
-    move (4,2);
-    prints("夢幣 %d ",money);
-    move (6,2);
-    prints("優良積分 %d ",point1);
-    if (!vget(b_lines,0,"請選擇您要的服務： [Q] 離開 ",buf,2,DOECHO))
+    move (4, 2);
+    prints("夢幣 %d ", money);
+    move (6, 2);
+    prints("優良積分 %d ", point1);
+    if (!vget(b_lines, 0, "請選擇您要的服務： [Q] 離開 ", buf, 2, DOECHO))
         return 0;
 
     if (*buf == '1')

so/bj.c
@@ -1,6 +1,6 @@
 #include "bbs.h"
 #include "gamef.c"
-#define SWAP(x,y) do {int temp=(x);(x)=(y);(y)=temp;} while (0)
+#define SWAP(x, y) do {int temp=(x);(x)=(y);(y)=temp;} while (0)
 
 /* 黑傑克遊戲 */
 
@@ -148,7 +148,7 @@ BlackJack(void)
                 cuser_money += (money * seven);
 //              inmoney(money*seven);
                 game_log(2, "中了 \x1b[1;33m%d\x1b[m 金幣的 \x1b[1;31m  ７７７   \x1b[m"
-                         , money*seven);
+                        , money*seven);
                 pressanykey("您還有 \x1b[1;44;33m%d\x1b[m 金幣", cuser_money);
                 flag = 1;m = 0;
             }
@@ -159,7 +159,7 @@ BlackJack(void)
                 move(3, 0);prints("\x1b[1;41;33m超級正統 BLACK JACK !!! 得獎金 %d 金幣\x1b[m", money*super_jack);
                 cuser_money += (money * super_jack);
                 game_log(2, "中了 \x1b[1;33m%d\x1b[m 金幣的 \x1b[1;41;33m 正統 ＡＪ \x1b[m"
-                         , money*super_jack);
+                        , money*super_jack);
                 pressanykey("您還有 \x1b[1;44;33m%d\x1b[m 金幣", cuser_money);
                 flag = 1;m = 0;
             }
@@ -183,7 +183,7 @@ BlackJack(void)
                 move(3, 0);prints("\x1b[1;41;33mBLACK JACK !!!\x1b[44m 得獎金 %d 金幣\x1b[m", money*win_jack);
                 cuser_money += (money * win_jack);
 //              inmoney(money*win_jack);
-                move(0, 0);clrtoeol();//prints("您還有 \x1b[1;44;33m%d\x1b[m 金幣",cuser.money);
+                move(0, 0);clrtoeol();//prints("您還有 \x1b[1;44;33m%d\x1b[m 金幣", cuser.money);
                 if (money*win_jack >= 500000)
                 {
                     game_log(2, "中了 \x1b[1;33m%d\x1b[m 金幣的 \x1b[1;47;30m BlackJack \x1b[m", money*win_jack);

so/xyz.c
@@ -31,7 +31,7 @@ x_siteinfo(void)
     prints("程式版本： %s [%s]\033[m\n", BBSVERNAME, BBSVERSION);
     prints("站上人數： %d/%d\n", ushm->count, MAXACTIVE);
     prints("系統負載： %.2f %.2f %.2f [%s]\n"
-        , load[0], load[1], load[2], load[0] > 10 ? "\033[1;41;37m過高\033[m" : load[0] > 5 ?
+       , load[0], load[1], load[2], load[0] > 10 ? "\033[1;41;37m過高\033[m" : load[0] > 5 ?
         "\033[1;42;37m稍高\033[m" : "\033[1;44;37m正常\033[m");
     prints("索引資料： BRD %d KB, ACCT %d KB, HDR %d KB\n", sizeof(BRD), sizeof(ACCT), sizeof(HDR));
     prints("\n");

util/poststat.c
@@ -227,10 +227,10 @@ poststat(
 
 #ifdef	DEBUG
             printf("Title : %s, Board: %s\nPostNo : %d, Author: %s\n"
-                ,pp->title
-                ,pp->board
-                ,pp->number
-                ,pp->author);
+               , pp->title
+               , pp->board
+               , pp->number
+               , pp->author);
 #endif
 
             j = sort(pp, j);
@@ -267,7 +267,7 @@ poststat(
             fprintf(fp,
                 "\x1b[1;31m%3d. \x1b[33m¬ÝªO : \x1b[32m%-16s\x1b[35m¡m %s¡n\x1b[36m%4d ½g\x1b[33m%16s\n"
                 "     \x1b[33m¼ÐÃD : \x1b[0;44;37m%-60.60s\x1b[40m\n"
-                ,++cnt, tp->board, p, tp->number, tp->author, tp->title);
+               , ++cnt, tp->board, p, tp->number, tp->author, tp->title);
         }
         fclose(fp);
     }

util/readBENZ.c
@@ -42,7 +42,7 @@ strcpy(buf, "/home/bbs/usr/p/pcbug/benz");
             while (read(fd, &bmw, sizeof(bmw)))
             {
                 printf("ID:%-13s sender:%-4d recver:%-4d msg:%s\n"
-                       , bmw.userid, bmw.sender, bmw.recver, bmw.msg);
+                      , bmw.userid, bmw.sender, bmw.recver, bmw.msg);
             }
 
 

commit d14c28a3f925a3c2f0f274d226ee006b0ce65a2e

Fix indentation before commas broken in 241b132.

maple/edit.c
@@ -1280,7 +1280,7 @@ ve_header(
                 HAS_PERM(PERM_DENYNICK) ? cutmp->username : cuser.username
 #endif
 
-               , curredit & EDIT_OUTGO ? str_post1 : str_post2, currboard);
+                , curredit & EDIT_OUTGO ? str_post1 : str_post2, currboard);
         }
     }
     fprintf(fp, "標題: %s\n時間: %s\n", title, ctime(&now));

maple/talk.c
@@ -2865,7 +2865,7 @@ static char *talk_reason[] =
     "你真的很煩，我實在不想跟你 talk"
 
 #ifdef EVERY_Z
-   , "我的嘴巴正忙著和別人講話呢，沒有空的嘴巴了"
+    , "我的嘴巴正忙著和別人講話呢，沒有空的嘴巴了"
     /* Thor.0725: for chat&talk 用^z 作準備 */
 #endif
 };
@@ -3103,7 +3103,7 @@ static char *msg_pickup_way[PICKUP_WAYS] =
     "暱稱",
     "閒置"
 #ifdef  HAVE_BOARD_PAL
-   , "板友"
+    , "板友"
 #endif
 };
 
@@ -3290,7 +3290,7 @@ static int (*ulist_cmp[]) (PICKUP *i, PICKUP *j) =
     ulist_cmp_nick,
     ulist_cmp_idle
 #ifdef  HAVE_BOARD_PAL
-   , ulist_cmp_board
+    , ulist_cmp_board
 #endif
 };
 

so/bank.c
@@ -66,7 +66,7 @@ int point1_money(void)
 
     move(2, 0);
     prints("你的身上有 %9d 夢幣\n\n           %9d 優良點數"
-                   , acct.money, acct.point1);
+           , acct.money, acct.point1);
     if (acct.point1 < 1)
     {
         pmsg2("優良點數不足");

so/bj.c
@@ -148,7 +148,7 @@ BlackJack(void)
                 cuser_money += (money * seven);
 //              inmoney(money*seven);
                 game_log(2, "中了 \x1b[1;33m%d\x1b[m 金幣的 \x1b[1;31m  ７７７   \x1b[m"
-                        , money*seven);
+                         , money*seven);
                 pressanykey("您還有 \x1b[1;44;33m%d\x1b[m 金幣", cuser_money);
                 flag = 1; m = 0;
             }
@@ -159,7 +159,7 @@ BlackJack(void)
                 move(3, 0); prints("\x1b[1;41;33m超級正統 BLACK JACK !!! 得獎金 %d 金幣\x1b[m", money*super_jack);
                 cuser_money += (money * super_jack);
                 game_log(2, "中了 \x1b[1;33m%d\x1b[m 金幣的 \x1b[1;41;33m 正統 ＡＪ \x1b[m"
-                        , money*super_jack);
+                         , money*super_jack);
                 pressanykey("您還有 \x1b[1;44;33m%d\x1b[m 金幣", cuser_money);
                 flag = 1; m = 0;
             }

so/pip.c
@@ -903,8 +903,8 @@ int mode)
         color1 = 37;
     sprintf(inbuf1, "%02d/%02d/%02d", (d.year - 11) % 100, d.month, d.day);
     sprintf(buf
-           , " \x1b[1;32m[狀  態]\x1b[37m %-5s     \x1b[32m[生  日]\x1b[37m %-9s \x1b[32m[年  齡]\x1b[37m %-5d     \x1b[32m[金  錢]\x1b[%dm %-8d \x1b[m"
-           , yo[age], inbuf1, tm, color1, d.money);
+            , " \x1b[1;32m[狀  態]\x1b[37m %-5s     \x1b[32m[生  日]\x1b[37m %-9s \x1b[32m[年  齡]\x1b[37m %-5d     \x1b[32m[金  錢]\x1b[%dm %-8d \x1b[m"
+            , yo[age], inbuf1, tm, color1, d.money);
     prints(buf);
 
     move(2, 0);
@@ -932,8 +932,8 @@ int mode)
         color3 = 37;
 
     sprintf(buf
-           , " \x1b[1;32m[生  命]\x1b[%dm %-10d\x1b[32m[法  力]\x1b[%dm %-10d\x1b[32m[體  重]\x1b[37m %-5d     \x1b[32m[疲  勞]\x1b[%dm %-4d\x1b[0m "
-           , color1, d.hp, color2, d.mp, d.weight, color3, d.tired);
+            , " \x1b[1;32m[生  命]\x1b[%dm %-10d\x1b[32m[法  力]\x1b[%dm %-10d\x1b[32m[體  重]\x1b[37m %-5d     \x1b[32m[疲  勞]\x1b[%dm %-4d\x1b[0m "
+            , color1, d.hp, color2, d.mp, d.weight, color3, d.tired);
     prints(buf);
 
     move(3, 0);
@@ -962,8 +962,8 @@ int mode)
     else
         color4 = 37;
     sprintf(buf
-           , " \x1b[1;32m[命 MAX]\x1b[37m %-10d\x1b[32m[法 MAX]\x1b[37m %-10d\x1b[32m[髒／病]\x1b[%dm %-4d\x1b[37m/\x1b[%dm%-4d \x1b[32m[快／滿]\x1b[%dm %-4d\x1b[37m/\x1b[%dm%-4d\x1b[m"
-           , d.maxhp, d.maxmp, color1, d.shit, color2, d.sick, color3, d.happy, color4, d.satisfy);
+            , " \x1b[1;32m[命 MAX]\x1b[37m %-10d\x1b[32m[法 MAX]\x1b[37m %-10d\x1b[32m[髒／病]\x1b[%dm %-4d\x1b[37m/\x1b[%dm%-4d \x1b[32m[快／滿]\x1b[%dm %-4d\x1b[37m/\x1b[%dm%-4d\x1b[m"
+            , d.maxhp, d.maxmp, color1, d.shit, color2, d.sick, color3, d.happy, color4, d.satisfy);
     prints(buf);
     if (mode == 0)  /*主要畫面*/
     {
@@ -1008,8 +1008,8 @@ int mode)
         else
             color4 = 37;
         sprintf(buf
-               , " \x1b[1;36m[食物]\x1b[%dm%-7d\x1b[36m[零食]\x1b[%dm%-7d\x1b[36m[補丸]\x1b[%dm%-7d\x1b[36m[靈芝]\x1b[%dm%-7d\x1b[36m[人參]\x1b[37m%-7d\x1b[36m[雪蓮]\x1b[37m%-7d\x1b[0m"
-               , color1, d.food, color2, d.cookie, color3, d.bighp, color4, d.medicine, d.ginseng, d.snowgrass);
+                , " \x1b[1;36m[食物]\x1b[%dm%-7d\x1b[36m[零食]\x1b[%dm%-7d\x1b[36m[補丸]\x1b[%dm%-7d\x1b[36m[靈芝]\x1b[%dm%-7d\x1b[36m[人參]\x1b[37m%-7d\x1b[36m[雪蓮]\x1b[37m%-7d\x1b[0m"
+                , color1, d.food, color2, d.cookie, color3, d.bighp, color4, d.medicine, d.ginseng, d.snowgrass);
         prints(buf);
 
     }
@@ -1017,8 +1017,8 @@ int mode)
     {
         move(4, 0);
         sprintf(buf
-               , " \x1b[1;36m[愛心]\x1b[37m%-5d\x1b[36m[智慧]\x1b[37m%-5d\x1b[36m[氣質]\x1b[37m%-5d\x1b[36m[藝術]\x1b[37m%-5d\x1b[36m[道德]\x1b[37m%-5d\x1b[36m[勇敢]\x1b[37m%-5d\x1b[36m[家事]\x1b[37m%-5d\x1b[0m"
-               , d.love, d.wisdom, d.character, d.art, d.ethics, d.brave, d.homework);
+                , " \x1b[1;36m[愛心]\x1b[37m%-5d\x1b[36m[智慧]\x1b[37m%-5d\x1b[36m[氣質]\x1b[37m%-5d\x1b[36m[藝術]\x1b[37m%-5d\x1b[36m[道德]\x1b[37m%-5d\x1b[36m[勇敢]\x1b[37m%-5d\x1b[36m[家事]\x1b[37m%-5d\x1b[0m"
+                , d.love, d.wisdom, d.character, d.art, d.ethics, d.brave, d.homework);
         prints(buf);
 
     }
@@ -1026,8 +1026,8 @@ int mode)
     {
         move(4, 0);
         sprintf(buf
-               , " \x1b[1;36m[智慧]\x1b[37m%-5d\x1b[36m[氣質]\x1b[37m%-5d\x1b[36m[藝術]\x1b[37m%-5d\x1b[36m[勇敢]\x1b[37m%-5d\x1b[36m[攻擊]\x1b[37m%-5d\x1b[36m[防禦]\x1b[37m%-5d\x1b[36m[速度]\x1b[37m%-5d\x1b[0m"
-               , d.wisdom, d.character, d.art, d.brave, d.attack, d.resist, d.speed);
+                , " \x1b[1;36m[智慧]\x1b[37m%-5d\x1b[36m[氣質]\x1b[37m%-5d\x1b[36m[藝術]\x1b[37m%-5d\x1b[36m[勇敢]\x1b[37m%-5d\x1b[36m[攻擊]\x1b[37m%-5d\x1b[36m[防禦]\x1b[37m%-5d\x1b[36m[速度]\x1b[37m%-5d\x1b[0m"
+                , d.wisdom, d.character, d.art, d.brave, d.attack, d.resist, d.speed);
         prints(buf);
 
     }
@@ -4915,7 +4915,7 @@ int first)
                     sprintf(buf, "普通攻擊，對方體力減低%d", dinjure);
                     vmsg(buf);
                     sprintf(buf, "\x1b[1;33m%s \x1b[37m施展了普通攻擊，\x1b[33m%s \x1b[37m的體力減低 \x1b[31m%d \x1b[37m點\x1b[m"
-                           , d.name, opponent->pip->name, dinjure);
+                            , d.name, opponent->pip->name, dinjure);
                 }
                 opponent->pip->resistmode = 0;
                 opponent->pip->msgcount++;
@@ -4950,7 +4950,7 @@ int first)
                         sprintf(buf, "全力攻擊，對方體力減低%d", dinjure);
                         vmsg(buf);
                         sprintf(buf, "\x1b[1;33m%s \x1b[37m施展了全力攻擊，\x1b[33m%s \x1b[37m的體力減低 \x1b[31m%d \x1b[37m點\x1b[m"
-                               , d.name, opponent->pip->name, dinjure);
+                                , d.name, opponent->pip->name, dinjure);
                     }
                     else
                     {
@@ -5012,7 +5012,7 @@ int first)
                             sprintf(buf, "魔法攻擊，對方體力減低%d", dinjure);
                             vmsg(buf);
                             sprintf(buf, "\x1b[1;33m%s \x1b[37m施展了魔法攻擊，\x1b[33m%s \x1b[37m的體力減低 \x1b[31m%d \x1b[37m點\x1b[m"
-                                   , d.name, opponent->pip->name, dinjure);
+                                    , d.name, opponent->pip->name, dinjure);
                         }
                     }
 
@@ -8659,8 +8659,8 @@ int mode)
         color1 = 37;
     sprintf(inbuf1, "%02d/%02d/%02d", (d.year - 11) % 100, d.month, d.day);
     sprintf(buf
-           , " \x1b[1;32m[狀  態]\x1b[37m %-5s     \x1b[32m[生  日]\x1b[37m %-9s \x1b[32m[年  齡]\x1b[37m %-5d     \x1b[32m[金  錢]\x1b[%dm %-8d \x1b[m"
-           , yo[age], inbuf1, m, color1, d.money);
+            , " \x1b[1;32m[狀  態]\x1b[37m %-5s     \x1b[32m[生  日]\x1b[37m %-9s \x1b[32m[年  齡]\x1b[37m %-5d     \x1b[32m[金  錢]\x1b[%dm %-8d \x1b[m"
+            , yo[age], inbuf1, m, color1, d.money);
     prints(buf);
 
     move(2, 0);
@@ -8690,8 +8690,8 @@ int mode)
     sprintf(inbuf1, "%d/%d", d.hp, d.maxhp);
     sprintf(inbuf2, "%d/%d", d.mp, d.maxmp);
     sprintf(buf
-           , " \x1b[1;32m[生  命]\x1b[%dm %-10s\x1b[32m[法  力]\x1b[%dm %-10s\x1b[32m[體  重]\x1b[37m %-5d     \x1b[32m[疲  勞]\x1b[%dm %-4d\x1b[0m "
-           , color1, inbuf1, color2, inbuf2, d.weight, color3, d.tired);
+            , " \x1b[1;32m[生  命]\x1b[%dm %-10s\x1b[32m[法  力]\x1b[%dm %-10s\x1b[32m[體  重]\x1b[37m %-5d     \x1b[32m[疲  勞]\x1b[%dm %-4d\x1b[0m "
+            , color1, inbuf1, color2, inbuf2, d.weight, color3, d.tired);
     prints(buf);
 
     move(3, 0);
@@ -8720,8 +8720,8 @@ int mode)
     else
         color4 = 37;
     sprintf(buf
-           , " \x1b[1;32m[髒  髒]\x1b[%dm %-4d      \x1b[32m[病  氣]\x1b[%dm %-4d      \x1b[32m[快樂度]\x1b[%dm %-4d      \x1b[32m[滿意度]\x1b[%dm %-4d\x1b[0m"
-           , color1, d.shit, color2, d.sick, color3, d.happy, color4, d.satisfy);
+            , " \x1b[1;32m[髒  髒]\x1b[%dm %-4d      \x1b[32m[病  氣]\x1b[%dm %-4d      \x1b[32m[快樂度]\x1b[%dm %-4d      \x1b[32m[滿意度]\x1b[%dm %-4d\x1b[0m"
+            , color1, d.shit, color2, d.sick, color3, d.happy, color4, d.satisfy);
     prints(buf);
     if (mode == 1)/*餵食*/
     {
@@ -8751,8 +8751,8 @@ int mode)
         else
             color4 = 37;
         sprintf(buf
-               , " \x1b[1;36m[食物]\x1b[%dm%-7d\x1b[36m[零食]\x1b[%dm%-7d\x1b[36m[補丸]\x1b[%dm%-7d\x1b[36m[靈芝]\x1b[%dm%-7d\x1b[36m[人參]\x1b[37m%-7d\x1b[36m[雪蓮]\x1b[37m%-7d\x1b[0m"
-               , color1, d.food, color2, d.cookie, color3, d.bighp, color4, d.medicine, d.ginseng, d.snowgrass);
+                , " \x1b[1;36m[食物]\x1b[%dm%-7d\x1b[36m[零食]\x1b[%dm%-7d\x1b[36m[補丸]\x1b[%dm%-7d\x1b[36m[靈芝]\x1b[%dm%-7d\x1b[36m[人參]\x1b[37m%-7d\x1b[36m[雪蓮]\x1b[37m%-7d\x1b[0m"
+                , color1, d.food, color2, d.cookie, color3, d.bighp, color4, d.medicine, d.ginseng, d.snowgrass);
         prints(buf);
 
     }

so/xyz.c
@@ -31,7 +31,7 @@ x_siteinfo(void)
     prints("程式版本： %s [%s]\033[m\n", BBSVERNAME, BBSVERSION);
     prints("站上人數： %d/%d\n", ushm->count, MAXACTIVE);
     prints("系統負載： %.2f %.2f %.2f [%s]\n"
-       , load[0], load[1], load[2], load[0] > 10 ? "\033[1;41;37m過高\033[m" : load[0] > 5 ?
+        , load[0], load[1], load[2], load[0] > 10 ? "\033[1;41;37m過高\033[m" : load[0] > 5 ?
         "\033[1;42;37m稍高\033[m" : "\033[1;44;37m正常\033[m");
     prints("索引資料： BRD %d KB, ACCT %d KB, HDR %d KB\n", sizeof(BRD), sizeof(ACCT), sizeof(HDR));
     prints("\n");

util/poststat.c
@@ -227,10 +227,10 @@ poststat(
 
 #ifdef  DEBUG
             printf("Title : %s, Board: %s\nPostNo : %d, Author: %s\n"
-               , pp->title
-               , pp->board
-               , pp->number
-               , pp->author);
+                , pp->title
+                , pp->board
+                , pp->number
+                , pp->author);
 #endif
 
             j = sort(pp, j);
@@ -267,7 +267,7 @@ poststat(
             fprintf(fp,
                 "\x1b[1;31m%3d. \x1b[33m¬ÝªO : \x1b[32m%-16s\x1b[35m¡m %s¡n\x1b[36m%4d ½g\x1b[33m%16s\n"
                 "     \x1b[33m¼ÐÃD : \x1b[0;44;37m%-60.60s\x1b[40m\n"
-               , ++cnt, tp->board, p, tp->number, tp->author, tp->title);
+                , ++cnt, tp->board, p, tp->number, tp->author, tp->title);
         }
         fclose(fp);
     }

util/readBENZ.c
@@ -42,7 +42,7 @@ strcpy(buf, "/home/bbs/usr/p/pcbug/benz");
             while (read(fd, &bmw, sizeof(bmw)))
             {
                 printf("ID:%-13s sender:%-4d recver:%-4d msg:%s\n"
-                      , bmw.userid, bmw.sender, bmw.recver, bmw.msg);
+                       , bmw.userid, bmw.sender, bmw.recver, bmw.msg);
             }
 
 

util/readFRIENZ.c
@@ -42,7 +42,7 @@ int main(void)
             while (read(fd, &bmw, sizeof(bmw)))
             {
                 printf("ID:%-13s sender:%-4d recver:%-4d msg:%s\n"
-                      , bmw.userid, bmw.sender, bmw.recver, bmw.msg);
+                       , bmw.userid, bmw.sender, bmw.recver, bmw.msg);
             }
 
 