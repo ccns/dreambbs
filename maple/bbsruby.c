1579bfefb2dc20c2c22c23732d96edd5b8d6eed8
IepIweidieng <37586669+IepIweidieng@users.noreply.github.com>
2019-02-12 14:05:53 +0800

    maple/bbsruby.c: TOC tag: Fix allowing only tags leading with 3 `#` instead of 2 as in the documentation.

--- maple/bbsruby.c
+++ maple/bbsruby.c
@@ -485,9 +485,14 @@ int ruby_script_range_detect(char **pStart, char **pEnd)
     // In this implement, we only allow TOC to be put BEFORE the actual script code
     while (tStart < cEnd)
     {
-        if (tStart[0] == '#' && tStart[1] == '#' && tStart[2] == '#')
+        if (tStart[0] == '#' && tStart[1] == '#')
         {
-            tStart += 3;
+            tStart += 2;
+
+            // The third '#' is optional
+            if (tStart[0] == '#')
+                tStart++;
+
             tEnd = tStart + 1;
             while (*tEnd != '\n') tEnd++;
 
