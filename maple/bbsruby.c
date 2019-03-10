01e42a2b286817e087799dd80832d07ea37267a0
IepIweidieng <37586669+IepIweidieng@users.noreply.github.com>
2019-03-11 06:36:14 +0800

    maple/bbsruby.c: Make BBS-Ruby able to compile on BBSs without `getyx()`.

--- maple/bbsruby.c
+++ maple/bbsruby.c
@@ -68,6 +68,14 @@ static const char str_ransi[] = "\x1b[m";
   #define vget(y, x, msg, buf, size, mode)  getdata(y, x, msg, buf, size, mode)
 #endif
 
+#ifndef BBSRUBY_HAVE_GETYX
+static inline void getyx(int *y, int *x)
+{
+    *y = cur_row;
+    *x = cur_col;
+}
+#endif
+
 static inline void getxy(int *x, int *y)
 {
     getyx(y, x);
