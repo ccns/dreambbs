d21cdcf692e98cce392341f4e6e260e2d5404bae
IepIweidieng <37586669+IepIweidieng@users.noreply.github.com>
2019-03-11 20:49:33 +0800

    scripts/: Refine formatting.
    * scripts/: Fix indentation.
    * scripts/: Remove redundant line ends.
    * scripts/: Add a space after `;`s if there is no one.
    * scripts/: Make `do` in the same line as `for`.

--- scripts/checkACCT.sh
+++ scripts/checkACCT.sh
@@ -2,25 +2,22 @@
 declare -i a
 declare -i b
 cd /home/bbs/run
-if (test -e NOACCT.log);then
-  rm NOACCT.log
+if (test -e NOACCT.log); then
+    rm NOACCT.log
 fi
 cd /home/bbs/usr
-for i in a b c d e f g h i j k l m n o p q r s t u v w x y z
-do
-        cd /home/bbs/usr/$i
-        for usr in $(ls)
-        do
-                cd /home/bbs/usr/$i/$usr
-                if !(test -e .ACCT);then
-                        echo $usr >> /home/bbs/run/NOACCT.log
-                        let a++
-                fi
-                let b++
-        done
+for i in a b c d e f g h i j k l m n o p q r s t u v w x y z; do
+    cd /home/bbs/usr/$i
+    for usr in $(ls); do
+        cd /home/bbs/usr/$i/$usr
+        if !(test -e .ACCT); then
+            echo $usr >> /home/bbs/run/NOACCT.log
+            let a++
+        fi
+        let b++
+    done
 done
 echo "$a user(s) have no ACCT" >> /home/bbs/run/NOACCT.log
 echo "$b user(s) is checked" >> /home/bbs/run/NOACCT.log
 #mail -s 'NOACCT.log' *********@gmail.com < /home/bbs/NOACCT.log
 #rm /home/bbs/NOACCT.log
-
--- scripts/clean.sh
+++ scripts/clean.sh
@@ -3,23 +3,21 @@ declare -i a
 declare -i b
 declare -i c
 cd /home/bbs/brd
-for file in $(ls)
-do
-        cd /home/bbs/brd/$file
-        if (test -e usies);then
-                rm -f usies
-                let a++
-        fi
-#        if (test -e brdstat);then
-#               rm -f brdstat
-#                let b++
-#        fi
-#        if (test -e bstatcount);then
-#               rm -f bstatcount
-#                let c++
-#        fi
+for file in $(ls); do
+    cd /home/bbs/brd/$file
+    if (test -e usies); then
+        rm -f usies
+        let a++
+    fi
+#    if (test -e brdstat); then
+#        rm -f brdstat
+#        let b++
+#    fi
+#    if (test -e bstatcount); then
+#        rm -f bstatcount
+#        let c++
+#    fi
 done
 echo "clean $a board usies"
 #echo "clean $b board brdstat"
 #echo "clean $c board bstatcount"
-
