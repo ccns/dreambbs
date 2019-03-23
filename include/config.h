a90454d1140b5c9a8b891db94db0fb492fba517b
IepIweidieng <37586669+IepIweidieng@users.noreply.github.com>
2019-03-24 00:15:09 +0800

    include/config.h: '(x, y)' -> '(y, x)': Fix a comment conflicts with the code.

--- include/config.h
+++ include/config.h
@@ -519,7 +519,7 @@
 
 #define MOVIE_LINES     (11)            /* 動畫最多有 11 列 */
 
-#define MENU_XPOS       23              /* 選單開始的 (x, y) 座標 */
+#define MENU_XPOS       23              /* 選單開始的 (y, x) 座標 */
 #define MENU_YPOS       13
 #define MENU_LOAD       1
 #define MENU_DRAW       2
