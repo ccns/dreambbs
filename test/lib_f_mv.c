6261eadeba1d39c0e6b0093d2b0901b212ba257f
holishing <holishingtest@gmail.com>
2019-04-20 00:25:24 +0800

    test/lib_f_mv: correct output message

--- test/lib_f_mv.c
+++ test/lib_f_mv.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-    printf("\x1b[1;33mCopying %s to %s ....\x1b[0m\n",argv[1],argv[2]);
+    printf("\x1b[1;33mMoving %s to %s ....\x1b[0m\n",argv[1],argv[2]);
     f_mv(argv[1],argv[2]);
     return 0;
 }
