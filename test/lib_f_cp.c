adbf37917d5b663d024e98ba48f12302b81f30a2
holishing <holishingtest@gmail.com>
2019-04-22 23:34:08 +0800

    test/lib_f_*: add argument check

--- test/lib_f_cp.c
+++ test/lib_f_cp.c
@@ -3,7 +3,12 @@
 
 int main(int argc, char *argv[])
 {
-    fprintf(stderr,"\x1b[1;33mCopying %s to %s ....\x1b[0m\n",argv[1],argv[2]);
-    f_cp(argv[1],argv[2],0600);
+    if(argc!=3) {
+        exit(1);
+    }
+    char *source = argv[1];
+    char *destination = argv[2];
+    fprintf(stderr,"\x1b[1;33mCopying %s to %s ....\x1b[0m\n",source,destination);
+    f_cp(source,destination,0600);
     return 0;
 }
