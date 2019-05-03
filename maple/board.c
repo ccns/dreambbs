ccns/dreambbs

commit ee73fa00e4e40d83e8d392ffeced65ed4f4415cc

maple/board.c: `brh_load()`: Avoid `memcpy()`ing 0 or less bytes.

maple/board.c
@@ -630,12 +630,12 @@ brh_load(void)
                     } while (n > 0);
 
                     head[2] = n;
-                }
 
-                n = n * sizeof(time_t) + sizeof(BRH);
-                if (base != head)
-                    memcpy(base, head, n);
-                base = (int *) ((char *) base + n);
+                    n = n * sizeof(time_t) + sizeof(BRH);
+                    if (base != head)
+                        memcpy(base, head, n);
+                    base = (int *) ((char *) base + n);
+                }
             }
             head += size;
         }

commit 29c3d83fe0c5216aeb1cca7eaabeede0f65e16a8

maple/board.c: `brh_load()`: Fix dropping BRH entries with no time tags, caused by ee73fa00e4e40d83e8d392ffeced65ed4f4415cc.

maple/board.c
@@ -631,12 +631,16 @@ brh_load(void)
                     } while (n > 0);
 
                     head[2] = n;
-
-                    n = n * sizeof(time_t) + sizeof(BRH);
-                    if (base != head)
-                        memcpy(base, head, n);
-                    base = (int *) ((char *) base + n);
                 }
+                else
+                {
+                    n = 0;
+                }
+
+                n = n * sizeof(time_t) + sizeof(BRH);
+                if (base != head)
+                    memcpy(base, head, n);
+                base = (int *) ((char *) base + n);
             }
             head += size;
         }