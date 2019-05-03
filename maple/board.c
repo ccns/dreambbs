69dc9c2cb9d0d71cec3a2c3981d02cc6d6471897
IepIweidieng <37586669+IepIweidieng@users.noreply.github.com>
2019-05-04 05:37:52 +0800

    Revert "maple/board.c: `brh_get()`: Improve readability of code for moving the current board's BRH out of all BRHs."
    This reverts commit d1b9802977410150878f9ad1f08ca9f7379aefe7.
    Fix `brh_tail` being misplaced after getting current BRH entry.

--- maple/board.c
+++ maple/board.c
@@ -129,8 +129,9 @@ brh_get(
             {
                 bcnt = item;
                 memcpy(buf, head + 3, size - 3 * sizeof(time_t));
+                tail = (int *) ((char *) tail - size);
                 if ((item = (char *) tail - (char *) head))
-                    memcpy(head, (char *) head + size, item - size);
+                    memcpy(head, (char *) head + size, item);
                 break;
             }
             head = (int *) ((char *) head + size);
