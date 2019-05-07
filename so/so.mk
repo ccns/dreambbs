7d40516de5e01c1b8bca2d0994aa937eeeb62757
IepIweidieng <37586669+IepIweidieng@users.noreply.github.com>
2019-05-07 23:00:19 +0800

    so/Makefile: Move target definitions into new file 'so/so.mk'.

---
+++ so/so.mk
@@ -0,0 +1,17 @@
+# -------------------------------------------------------#
+#  so/so.mk     ( NCKU CCNS WindTop-DreamBBS 2.0 )       #
+# -------------------------------------------------------#
+#  Author: 37586669+IepIweidieng@users.noreply.github.com#
+#  Target: Shared object target files for DreamBBS       #
+#  Create: 2019/05/07                                    #
+# -------------------------------------------------------#
+
+HDR	= bbs.h config.h global.h modes.h perm.h struct.h bbs_script.h cppdef.h
+
+SO	= chat.so vote.so xyz.so guessnum.so \
+	  admin.so chatmenu.so  mailgem.so\
+	  memorandum.so aloha.so newboard.so violate.so song.so same_mail.so\
+	  showvote.so list.so mine.so bj.so \
+	  pnote.so passwd.so adminutil.so ascii.so\
+	  classtable2.so observe.so pip.so brdstat.so personal.so \
+	  cleanrecommend.so shop.so bank.so innbbs.so contact.so
