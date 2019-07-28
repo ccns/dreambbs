143202a8e38da284bc057f918a5f6b29fca876fc
holishing <holishingtest@gmail.com>
2019-07-28 17:17:01 +0800

    add f_rm() test script

---
+++ test/lib_f_rm.c
@@ -0,0 +1,13 @@
+#include "bbs.h"
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    if(argc!=2) {
+        exit(1);
+    }
+    char *source = argv[1];
+    fprintf(stderr,"\x1b[1;33mRemoving %s ....\x1b[0m\n",source);
+    f_rm(source);
+    return 0;
+}
--- test/testscript.sh
+++ test/testscript.sh
@@ -23,3 +23,10 @@ sha256_before_cp=$(${SHA256} before_cp.txt |awk '{print $1}')
 sha256_after_cp=$(${SHA256} after_cp.txt |awk '{print $1}')
 test "$sha256_before_cp" = "$sha256_after_cp" && (>&2 printf "\033[1;32mtest f_cp ok!\033[0m\n")
 rm before_cp.txt after_cp.txt
+
+################### test f_rm()
+touch before_rm.txt
+printf "thisfileshouldbedeleted" >> before_rm.txt
+./lib_f_rm before_rm.txt
+test -e before_rm.txt && (>&2 printf "\033[1;31mtest f_rm failed!\033[0m\n" && false) || true
+(>&2 printf "\033[1;32mtest f_rm (for single file) ok!\033[0m\n")
