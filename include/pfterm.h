4e3f43015e998d1299ecc9f1dfab3c7c03093622
IepIweidieng <37586669+IepIweidieng@users.noreply.github.com>
2019-07-30 13:42:20 +0800

    Replace constant pointer function aliases with inlined forwarding functions
    - include/pfterm.h & maple/pfterm.c & maple/bbslua.c
    
    (cherry picked from commit 6788bf618c699adeeae2d22e6949de070e609183)

--- include/pfterm.h
+++ include/pfterm.h
@@ -83,9 +83,9 @@ void    grayout     (int y, int end, int level);
 // environment specific
 //////////////////////////////////////////////////////////////////////////
 void    scr_dump    (screen_backup_t *psb);
-extern void (*const scr_restore)  (screen_backup_t *psb);
-       void    scr_restore_free   (screen_backup_t *psb);
-       void    scr_restore_keep   (const screen_backup_t *psb);
+void    scr_restore_free   (screen_backup_t *psb);
+void    scr_restore_keep   (const screen_backup_t *psb);
+static inline void  scr_restore   (screen_backup_t *psb) { scr_restore_free(psb); }
 
 void move_ansi(int y, int x);
 
