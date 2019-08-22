4ff41aa707818d33ab8ac75ba5baf7973c7f7659
IepIweidieng <37586669+IepIweidieng@users.noreply.github.com>
2019-08-22 16:48:55 +0800

    maple/visio.c: Use `str_lowest()` to convert big5 strings to lowercase.
    (cherry picked from commit 88c3a976793e1c81b4680c12ab1df43f0710ffae)

--- maple/visio.c
+++ maple/visio.c
@@ -2246,17 +2246,7 @@ int vget(int line, int col, const char *prompt, char *data, int max, int echo)
     outc('\n');
 
     if (echo & LCECHO)
-    {
-        for (col = 0; col < len; col++)
-        {
-            ch = (unsigned char) data[col];
-            if (ch >= 0x81 && ch <= 0xFE)
-                /* The first byte of double-byte char of Big5 */
-                col++;  /* Skip the next byte */
-            else if (ch >= 'A' && ch <= 'Z')
-                data[col] += 32;
-        }
-    }
+        str_lowest(data, data);
     ch = (unsigned char) data[0];
 
 #ifdef M3_USE_PFTERM
