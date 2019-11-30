0b116f403e65289f47750263e7fd4e02531f62dc
IepIweidieng <37586669+IepIweidieng@users.noreply.github.com>
2019-12-01 03:56:50 +0800

    CMake: Create `CMakeLists.txt` for building only.

---
+++ so/so.cmake
@@ -0,0 +1,17 @@
+# -------------------------------------------------------#
+#  so/so.cmake    ( NCKU CCNS WindTop-DreamBBS 3.0 )     #
+# -------------------------------------------------------#
+#  Author: 37586669+IepIweidieng@users.noreply.github.com#
+#  Target: Shared libraries for DreamBBS                 #
+#  Create: 2019/11/30                                    #
+# -------------------------------------------------------#
+
+set(SO
+    chat vote xyz guessnum
+    admin chatmenu mailgem
+    memorandum aloha newboard violate song same_mail
+    showvote list mine bj
+    pnote passwd adminutil ascii
+    classtable2 observe pip brdstat personal
+    cleanrecommend shop bank innbbs contact
+)
