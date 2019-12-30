2b9c1ef96c409c8831b5fe54a7e7761da565fdd3
IepIweidieng <37586669+IepIweidieng@users.noreply.github.com>
2019-12-31 06:02:47 +0800

    include/cppdef.h: Shorten `DL_PATCH_STR` from `DlPatch` to `DL`.

--- include/cppdef.h
+++ include/cppdef.h
@@ -50,7 +50,7 @@
 
 /* Macros for acquiring version strings from number literals */
 
-#define DL_PATCH_STR  "DlPatch"
+#define DL_PATCH_STR  "DL"
 #define DL_PATCH_SEP  "-" DL_PATCH_STR "-"
 
 // major . minor
@@ -60,7 +60,7 @@
 #define VER_PATCH_STR(major, minor, patch) \
     VER_STR(major, minor) "." CPP_STR(CPP_UNPAREN_OPT(patch))
 
-// ver -DlPatch- dl_patch
+// ver -DL- dl_patch
 // `ver` is a string
 #define VERSION_STR_DL_PATCH_STR(version_str, dl_patch) \
     version_str DL_PATCH_SEP CPP_STR(CPP_UNPAREN_OPT(dl_patch))
@@ -69,11 +69,11 @@
 #define VERSION_DL_PATCH_STR(version, dl_patch) \
     VERSION_STR_DL_PATCH_STR(CPP_STR(CPP_UNPAREN_OPT(version)), dl_patch)
 
-// major . minor -DlPatch- dl_patch
+// major . minor -DL- dl_patch
 #define VER_DL_STR(major, minor, dl_patch) \
     VERSION_STR_DL_PATCH_STR(VER_STR(major, minor), dl_patch)
 
-// major . minor . patch -DlPatch- dl_patch
+// major . minor . patch -DL- dl_patch
 #define VER_PATCH_DL_STR(major, minor, patch, dl_patch) \
     VERSION_STR_DL_PATCH_STR(VER_PATCH_STR(major, minor, patch), dl_patch)
 
