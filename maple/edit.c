ccns/dreambbs

commit d52ad44f29c75765ce573833fed2a9eeb02620ff

maple/edit.c: `vedit()`: `ve_key`: Allow backspaces and refine deletes in ANSI mode.

maple/edit.c
@@ -1875,14 +1875,11 @@ ve_key:
 
             case Ctrl('H'):             /* backspace */
 
-                /* Thor: 在 ANSI 編輯模式下, 不可以按倒退, 不然會很可怕.... */
-
-                if (mode & VE_ANSI)
-                    goto ve_key;
 
                 if (col)
                 {
-                    delete_char(vln, ve_col = --col);
+                    ve_col = (mode & VE_ANSI) ? ansi2n(pos - 1, vln) : col - 1;
+                    delete_char(vln, ve_col);
                     continue;
                 }
 
@@ -1912,9 +1909,9 @@ ve_key:
                 {
                     if (cc == 0)
                         goto ve_key;
-                    delete_char(vln, col);
-                    if (mode & VE_ANSI) /* Thor: 雖然增加 load, 不過edit 時會比較好看 */
-                        ve_col = ansi2n(n2ansi(col, vln), vln);
+                    /* Thor: 雖然增加 load, 不過edit 時會比較好看 */
+                    ve_col = (mode & VE_ANSI) ? ansi2n(pos, vln) : pos;
+                    delete_char(vln, ve_col);
                 }
                 continue;
 

commit 6d987831f9f3fb098b7f4f0110de7016bc8f6ff5

maple/edit.c: `vedit()`: `ve_key`: Fix line deletion in ANSI mode broken since d52ad44f29c75765ce573833fed2a9eeb02620ff.
- Fix the current line not deleted
   when pressing backspace at the line beginning in ANSI mode
      and there exist any ANSI escapes
   - Fix `ve_col` becoming negative when this occurs
- Fix the current line not deleted
   when pressing delete at the line end in ANSI mode
      and there exist any ANSI escapes

maple/edit.c
@@ -1876,7 +1876,7 @@ ve_key:
             case Ctrl('H'):             /* backspace */
 
 
-                if (col)
+                if ((mode & VE_ANSI) ? pos : col)
                 {
                     ve_col = (mode & VE_ANSI) ? ansi2n(pos - 1, vln) : col - 1;
                     delete_char(vln, ve_col);
@@ -1899,8 +1899,8 @@ ve_key:
             case Ctrl('D'):
             case KEY_DEL:               /* delete current character */
 
-                cc = vln->len;
-                if (cc == col)
+                cc = (mode & VE_ANSI) ? n2ansi(vln->len, vln) : vln->len;
+                if (cc == ((mode & VE_ANSI) ? pos : col))
                 {
                     join_up(vln);
                     ve_mode = mode | VE_REDRAW;