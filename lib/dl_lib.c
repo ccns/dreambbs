ccns/dreambbs

commit f585aa27fcc47ecddc5c6555a4d773ea029071af

C++ compat: Make type casts from `void *` explicit.

lib/dl_lib.c
@@ -82,7 +82,7 @@ int DL_func(const char *name, ...)
 
     va_start(args, name);
 
-    if (!(f = DL_get(name)))
+    if (!(f = (int (*)(va_list)) DL_get(args)))
     {                            /* not get func */
         va_end(args);
         return -1;

innbbsd/bbslink.c
@@ -69,7 +69,7 @@ search_nodelist_bynode(
     nodelist_t nl;
 
     str_ncpy(nl.name, name, sizeof(nl.name));
-    return bsearch(&nl, NODELIST, NLCOUNT, sizeof(nodelist_t), nl_bynamecmp);
+    return (nodelist_t *) bsearch(&nl, NODELIST, NLCOUNT, sizeof(nodelist_t), nl_bynamecmp);
 }
 
 
@@ -80,7 +80,7 @@ search_newsfeeds_byboard(
     newsfeeds_t nf;
 
     str_ncpy(nf.board, board, sizeof(nf.board));
-    return bsearch(&nf, NEWSFEEDS_B, NFCOUNT, sizeof(newsfeeds_t), nf_byboardcmp);
+    return (newsfeeds_t *) bsearch(&nf, NEWSFEEDS_B, NFCOUNT, sizeof(newsfeeds_t), nf_byboardcmp);
 }
 
 

innbbsd/inntobbs.c
@@ -83,7 +83,7 @@ header_value(
     }
 
     key.name = inputheader;
-    findkey = bsearch(&key, (char *) headertable, sizeof(headertable) / sizeof(header_t), sizeof(key), header_cmp);
+    findkey = (header_t *) bsearch(&key, (char *) headertable, sizeof(headertable) / sizeof(header_t), sizeof(key), header_cmp);
     if (findkey != NULL)
         return findkey->id;
 

innbbsd/rec_article.c
@@ -37,7 +37,7 @@ init_bshm(void)
     /* itoc.030727: 在開啟 bbsd 之前，應該就要執行過 account，
        所以 bshm 應該已設定好 */
 
-    bshm = shm_new(BRDSHM_KEY, sizeof(BCACHE));
+    bshm = (BCACHE *) shm_new(BRDSHM_KEY, sizeof(BCACHE));
 
     if (bshm->uptime <= 0)      /* bshm 未設定完成 */
         exit(0);
@@ -452,7 +452,7 @@ search_newsfeeds_bygroup(
     newsfeeds_t nf, *find;
 
     str_ncpy(nf.newsgroup, newsgroup, sizeof(nf.newsgroup));
-    find = bsearch(&nf, NEWSFEEDS_G, NFCOUNT, sizeof(newsfeeds_t), nf_bygroupcmp);
+    find = (newsfeeds_t *) bsearch(&nf, NEWSFEEDS_G, NFCOUNT, sizeof(newsfeeds_t), nf_bygroupcmp);
     if (find && !(find->xmode & INN_NOINCOME))
         return find;
     return NULL;

lib/record.c
@@ -31,7 +31,7 @@ int rec_bot(                    /* amaki.040715: 嵌入式寫檔 */
                const char *fpath, const void *data, int size)
 {
     int fd, fsize, count;
-    void *pool = NULL, *set;
+    HDR *pool = NULL, *set;
     char set_pool[REC_SIZ];
     struct stat st;
 
@@ -45,7 +45,7 @@ int rec_bot(                    /* amaki.040715: 嵌入式寫檔 */
     fstat(fd, &st);
 
     count = 0;
-    set = (void *)set_pool;
+    set = (HDR *)set_pool;
 
     if ((fsize = st.st_size))
     {
@@ -57,8 +57,7 @@ int rec_bot(                    /* amaki.040715: 嵌入式寫檔 */
             {
                 if (count)
                 {
-                    pool = (void *)malloc(count * size);
-
+                    pool = (HDR *)malloc(count * size);
                     read(fd, pool, count * size);
                     lseek(fd, -size * count, SEEK_CUR);
                 }
@@ -67,7 +66,7 @@ int rec_bot(                    /* amaki.040715: 嵌入式寫檔 */
             else if (fsize <= 0)    /* amaki.040715: 全部都是置底的東西 */
             {
                 count++;
-                pool = (void *)malloc(count * size);
+                pool = (HDR *)malloc(count * size);
 
                 lseek(fd, -size, SEEK_CUR);
                 read(fd, pool, count * size);

lib/xsort.c
@@ -70,7 +70,7 @@ void xsort(void *a, size_t n, size_t es, int (*cmp) (const void *lhs, const void
 
     if (n > 7)
     {
-        pl = a;
+        pl = (char *)a;
         pn = (char *)a + (n - 1) * es;
         if (n > 40)
         {
@@ -81,7 +81,7 @@ void xsort(void *a, size_t n, size_t es, int (*cmp) (const void *lhs, const void
         }
         pm = med3(pl, pm, pn, cmp);
     }
-    swap(a, pm);
+    swap((char *)a, pm);
     pa = pb = (char *)a + es;
 
     pc = pd = (char *)a + (n - 1) * es;
@@ -125,7 +125,7 @@ void xsort(void *a, size_t n, size_t es, int (*cmp) (const void *lhs, const void
 
     pn = (char *)a + n * es;
     r = min(pa - (char *)a, pb - pa);
-    vecswap(a, pb - r, r);
+    vecswap((char *)a, pb - r, r);
 
     r = min(pd - pc, pn - pd - es);
     vecswap(pb, pn - r, r);

maple/acct.c
@@ -1066,7 +1066,7 @@ void acct_setup(ACCT * u, int adm)
             case 'i':
                 if (!sm)
                 {
-                    sm = DL_GET(DL_NAME("same_mail.so", same_mail));
+                    sm = (int (*) (char *mail)) DL_GET(DL_NAME("same_mail.so", same_mail));
                 }
                 strcpy(id, u->userid);
                 if (sm)

maple/bbsd.c
@@ -889,7 +889,7 @@ tn_login(void)
                 char fpath[128];
                 usr_fpath(fpath, cuser.userid, "gem");
 
-                p = DL_GET(DL_NAME("mailgem.so", gcheck));
+                p = (int (*)(int level, char *fpath)) DL_GET(DL_NAME("mailgem.so", gcheck));
                 if (p)
                     (*p)(0, fpath);
             }

commit 77bf73e8c3c91dc061c26dac2edfa5d68e837157

DSO: lib/dl_lib.c: Refine code.
- Refine `dlopen()` calls with the new macro `DL_OPEN_FLAGS`
- `DL_get()`: Use lengthed string operation to eliminate indirect buffers.
- Use posix `PATH_MAX` defined in `<limits.h>` for the limit of module path length
- Refactor `DL_get()` into several functions.
- `DL_insert()`: Simplify allocation logic.
- `DL_func()`:
   - Fix the wrong argument to `DL_get()`, caused by f585aa27fcc47ecddc5c6555a4d773ea029071af.
   - Move `DL_get()` to the front of `va_start()` to eliminate a `va_end()`

lib/dl_lib.c
@@ -8,6 +8,7 @@
 #include "config.h"
 
 #include <dlfcn.h>
+#include <limits.h>
 #include <stdarg.h>
 #include <strings.h>
 #include <stdlib.h>
@@ -26,72 +27,80 @@ int dl_size, dl_head;
 
 #define DL_ALLOC_MIN    5
 
-void *DL_get(const char *name
-             /* format: "Xmodule_path:Xname" */
-    )
-{
-    char buf[512], *t;
-    DL_list *p, *tail;
-
-    strcpy(buf, name);
-    if (!(t = (char *)strchr(buf, ':')))
-        return NULL;
-
-    *t++ = 0;
-
-    if (!dl_pool)
-    {
-        /* Initialize DL entries */
-        dl_size = DL_ALLOC_MIN;
-        /* dl_head = 0 */
-        dl_pool = (DL_list *) malloc(dl_size * sizeof(DL_list));
-    }
-
-    p = dl_pool;
-    tail = p + dl_head;
-    while (p < tail)
-    {
-        if (!strcmp(buf, p->path))
-            break;
-        p++;
-    }
-
-    if (p >= tail)
-    {                            /* not found */
-        if (dl_head >= dl_size)
-        {                        /* not enough space */
-            dl_size += DL_ALLOC_MIN;
-            dl_pool = (DL_list *) realloc(dl_pool, dl_size * sizeof(DL_list));
-            p = dl_pool + dl_head;    /* visor.20000718: to a new place */
-        }
 #if defined(__OpenBSD__)
-        p->handle = dlopen(p->path = (char *)strdup(buf), RTLD_LAZY);
+  #define DL_OPEN_FLAGS  RTLD_LAZY
 #else
-        p->handle = dlopen(p->path = (char *)strdup(buf), RTLD_NOW);
+  #define DL_OPEN_FLAGS  RTLD_NOW
 #endif
+
+/* `DL_list` manipulations */
+
+static DL_list *DL_find(const char *path, int path_len)
+{
+    const DL_list *const tail = dl_pool + dl_head;
+    DL_list *p;
+    for (p = dl_pool; p < tail; p++)
+    {
+        if (!strncmp(path, p->path, path_len))
+            break;
+    }
+    return p;
+}
+
+static DL_list *DL_insert(const char *path, int path_len)
+{
+    DL_list *p;
+
+    if (dl_head >= dl_size)
+    {                            /* not enough space or not initialized */
+        dl_size += DL_ALLOC_MIN;
+        dl_pool = (DL_list *) realloc(dl_pool, dl_size * sizeof(DL_list));
+    }
+
+    p = DL_find(path, path_len);
+    if (p >= dl_pool + dl_head)
+    {                            /* not found */
+        p->path = strndup(path, path_len);
+        p->handle = dlopen(p->path, DL_OPEN_FLAGS);
         dl_head++;
     }
 
+    return p;
+}
+
+/* Dynamic library object getters */
+
+/* format: "Xmodule_path:Xname" */
+static inline char *DL_name_delim(const char *name)
+{
+    return (char *)memchr(name, ':', strnlen(name, PATH_MAX));
+}
+
+void *DL_get(const char *name)
+{
+    const char *t = DL_name_delim(name);
+    DL_list *p;
+    if (!t)
+        return NULL;
+
+    p = DL_insert(name, t - name);
     if (!p->handle)
         return NULL;
 
-    return dlsym(p->handle, t);
+    return dlsym(p->handle, t+1);
 }
 
 int DL_func(const char *name, ...)
 {
     va_list args;
-    int (*f) (va_list), ret;
+    int (*f)(va_list) = (int (*)(va_list)) DL_get(name);
+    int ret;
 
-    va_start(args, name);
-
-    if (!(f = (int (*)(va_list)) DL_get(args)))
-    {                            /* not get func */
-        va_end(args);
+    if (!f)                      /* not get func */
         return -1;
-    }
 
-    ret = (*f) (args);
+    va_start(args, f);
+    ret = (*f)(args);
     va_end(args);
 
     return ret;