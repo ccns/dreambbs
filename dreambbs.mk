84c3137d9e6a3771150b580b70ad09093961c96b
Wei-Cheng Yeh (IID) <iid@ccns.ncku.edu.tw>
2021-02-25 01:12:29 +0800

    chore(makefiles): make implicit conversion between pointer and integer compile-time errors

--- dreambbs.mk
+++ dreambbs.mk
@@ -24,7 +24,7 @@ BUILDTIME	!= date '+%s'
 
 ## To be expanded
 
-CFLAGS_WARN	= -Wall -Wpointer-arith -Wcast-qual -Wwrite-strings -Werror=format -Werror=incompatible-pointer-types
+CFLAGS_WARN	= -Wall -Wpointer-arith -Wcast-qual -Wwrite-strings -Werror=format -Werror=incompatible-pointer-types -Werror=int-conversion
 CFLAGS_MK	= -ggdb3 -O0 -pipe $(CFLAGS_WARN) -I$$(SRCROOT)/include $(CFLAGS_ARCHI) $(CFLAGS_COMPAT)
 
 LDFLAGS_MK = -L$$(SRCROOT)/lib -ldao -lcrypt $(LDFLAGS_ARCHI)
