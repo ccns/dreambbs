0922fc2eda28259c14d57ce907bd646c406f5f35
Wei-Cheng Yeh (IID) <iid@ccns.ncku.edu.tw>
2021-03-06 02:33:42 +0800

    chore(version): bump version to DreamBBS-202X v0-Azure
    * include/config.h:
        * Update definition of BBSVERNAME & BBSVERSION

--- include/config.h
+++ include/config.h
@@ -35,8 +35,8 @@
 #define MYHOSTNAME      "ccns.cc"                   /* 網路位址 */
 #endif
 
-#define BBSVERNAME      "DreamBBS"                  /* 版本名稱 */
-#define BBSVERSION      "v3.0-CURRENT"              /* 版本編號, 最多 <= 14 字 */
+#define BBSVERNAME      "DreamBBS-202X"             /* 版本名稱 */
+#define BBSVERSION      "v0-Azure"                  /* 版本編號, 最多 <= 14 字 */
 #define BBSVER_SUFFIX   ""                          /* A version string for the path suffix */
 #define BINARY_SUFFIX   "bin" BBSVER_SUFFIX "/"     /* todo: move it to global.h, or merge global.h to here */
 
