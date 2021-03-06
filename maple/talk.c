cae345e2bb2d3bc367534c26871f45d3fd16b779
Wei-Cheng Yeh (IID) <iid@ccns.ncku.edu.tw>
2021-03-06 13:15:38 +0800

    feat(user list): limit user name field length for larger user address info field
    * maple/talk.c ulist_neck() & ulist_body():
        * Limit the length of the field for user name
    * Change output format to refactor out the additional additions and subtractions
        * include/theme.h NECK_ULIST
        * maple/talk.c ulist_neck() & ulist_body()

--- maple/talk.c
+++ maple/talk.c
@@ -3188,12 +3188,17 @@ ulist_body(
 
                 strcpy(color, wcolor[fcolor]);
 
-                prints("%6d%c%s%-*s %-*.*s%s%-*.*s%c%c %-*.*s %5.5s",
+                /* IID.2021-03-06: Limit the length of the field for user name to make the field for user address information larger */
+                const int margin = BMAX((d_cols >> 1) + 21 - (int)(unsigned)BMAX(sizeof(up->realname), sizeof(up->username)), 0);
+                const int name_len = (d_cols >> 1) + 21 - margin;
+                const int from_len = ((d_cols+1) >> 1) + 15 + margin;
+
+                prints("%6d%c%s%-*s %-*.*s %s%-*.*s %c%c %-*.*s %5.5s",
                     cnt, (up->ufo & UFO_WEB)?'*':' ',
                     color, IDLEN, up->userid,
-                    (d_cols >> 1) + 22, (d_cols >> 1) + 21, (HAS_PERM(PERM_SYSOP) && (cuser.ufo2 & UFO2_REALNAME))? up->realname : up->username,
+                    name_len, name_len, (HAS_PERM(PERM_SYSOP) && (cuser.ufo2 & UFO2_REALNAME))? up->realname : up->username,
                     colortmp > 0 ? "\x1b[m" : "",
-                    ((d_cols+1) >> 1) + 16, ((d_cols+1) >> 1) + 15,
+                    from_len, from_len,
                     (cuser.ufo2 & UFO2_SHIP) ? ship : ((up->ufo & UFO_HIDDEN)&&!HAS_PERM(PERM_SYSOP)) ?
                     HIDDEN_SRC : up->from, diff, diffmsg,
                     IDLEN, IDLEN, bmode(up, 0), buf);
@@ -3404,6 +3409,11 @@ static int
 ulist_neck(
     XO *xo)
 {
+    /* IID.2021-03-06: Limit the length of the field for user name to make the field for user address information larger */
+    const int margin = BMAX((d_cols >> 1) + 21 - (int)(unsigned)BMAX(MEMBER_SIZE(ACCT, realname), MEMBER_SIZE(ACCT, username)), 0);
+    const int name_len = (d_cols >> 1) + 21 - margin;
+    const int from_len = ((d_cols+1) >> 1) + 15 + margin;
+
     move(1, 0);
     prints("  排列方式：[\x1b[1m%s\x1b[m] 上站人數：%d %s我的朋友：%d %s與我為友：%d %s壞人：%d"
         IF_ON(HAVE_BOARD_PAL, " \x1b[0;36m板友：%d", "")
@@ -3414,8 +3424,8 @@ ulist_neck(
 #endif
         );
     prints(NECK_ULIST,
-        (d_cols >> 1) + 22, (HAS_PERM(PERM_SYSOP) && (cuser.ufo2 & UFO2_REALNAME)) ? "真實姓名" : "暱  稱",
-        ((d_cols+1) >> 1) + 13, (cuser.ufo2 & UFO2_SHIP) ? "好友描述" :"故鄉", "動態");
+        name_len, (HAS_PERM(PERM_SYSOP) && (cuser.ufo2 & UFO2_REALNAME)) ? "真實姓名" : "暱  稱",
+        from_len, (cuser.ufo2 & UFO2_SHIP) ? "好友描述" :"故鄉", "動態");
 
     return ulist_body(xo);
 }
