ccns/dreambbs

commit aa8f71db2ae0e238edf0654fcf03aafb89df6ef4

chore(bbsd argv_default[]): change default ports for better example

maple/bbsd.c
@@ -40,11 +40,7 @@ static const char *argv_default[] = {
     NULL, /* For `argv[0]`, the name of the program */
     "23",
     "3456",
-    "3001",
-/*
-    "3002",
-    "3003",
-*/
+    "-u", "run/bbsd.sock",
 };
 
 /* Thor.990113: exports for anonymous log */

commit c078ca4561696bb8346ee7e8504c4bb2d28452d6

chore(bbsd): "bbsd.sock" -> "bbsd.socket": make the default arg consistent with "bbsd_unixsocket.service"
The inconsistency was introduced in aa8f71db2ae0e238edf0654fcf03aafb89df6ef4

* maple/bbsd.c argv_default[]

maple/bbsd.c
@@ -40,7 +40,7 @@ static const char *argv_default[] = {
     NULL, /* For `argv[0]`, the name of the program */
     "23",
     "3456",
-    "-u", "run/bbsd.sock",
+    "-u", "run/bbsd.socket",
 };
 
 /* Thor.990113: exports for anonymous log */