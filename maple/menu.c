ccns/dreambbs

commit 3acbab2dac434dd2219731b691627d1635f42217

perf(MenuItem): make members funcarg & dlfuncarg pointers
This reduces the size of MenuItem to the size of a single pointer,
which was the size before the development of DreamBBS v3.0.

* include/struct.h union MenuItem:
    * Change members funcarg & dlfuncarg to pointers

* Add dedicated handling logic for `M_DL(M_*) | M_ARG`
    * maple/menu.c:
        * domenu_exec()
    * maple/popupmenu.c:
        * do_cmd()

* Use new macros FUNCARG() & DLFUNCARG() for simplifying MENU initialization
    * include/struct.h (definitions)
    * maple/menu.c:
        * menu_reset[]
        * menu_information[]
        * menu_xyz[]
    * maple/xover.c:
        * menu_everyz[]

* maple/acct.c xfile():
    * Use new local funcargs[] to keep the FuncArg objects

maple/menu.c
@@ -541,25 +541,25 @@ INTERNAL_INIT MENU menu_settingadm[] =
 /* ----------------------------------------------------- */
 INTERNAL_INIT MENU menu_reset[] =
 {
-    {{.dlfuncarg = {{DL_NAME("adminutil.so", m_resetsys)}, (const void *)1}}, PERM_BOARD, M_DL(M_XMODE) | M_ARG,
+    {{DLFUNCARG(DL_NAME("adminutil.so", m_resetsys), 1)}, PERM_BOARD, M_DL(M_XMODE) | M_ARG,
     "Camera     動態看板"},
 
-    {{.dlfuncarg = {{DL_NAME("adminutil.so", m_resetsys)}, (const void *)2}}, PERM_BOARD, M_DL(M_XMODE) | M_ARG,
+    {{DLFUNCARG(DL_NAME("adminutil.so", m_resetsys), 2)}, PERM_BOARD, M_DL(M_XMODE) | M_ARG,
     "Group      分類群組"},
 
-    {{.dlfuncarg = {{DL_NAME("adminutil.so", m_resetsys)}, (const void *)3}}, PERM_SYSOP, M_DL(M_XMODE) | M_ARG,
+    {{DLFUNCARG(DL_NAME("adminutil.so", m_resetsys), 3)}, PERM_SYSOP, M_DL(M_XMODE) | M_ARG,
     "Mail       寄信收信轉信"},
 
-    {{.dlfuncarg = {{DL_NAME("adminutil.so", m_resetsys)}, (const void *)4}}, PERM_ADMIN, M_DL(M_XMODE) | M_ARG,
+    {{DLFUNCARG(DL_NAME("adminutil.so", m_resetsys), 4)}, PERM_ADMIN, M_DL(M_XMODE) | M_ARG,
     "Killbbs    清除不正常 BBS"},
 
-    {{.dlfuncarg = {{DL_NAME("adminutil.so", m_resetsys)}, (const void *)5}}, PERM_BOARD, M_DL(M_XMODE) | M_ARG,
+    {{DLFUNCARG(DL_NAME("adminutil.so", m_resetsys), 5)}, PERM_BOARD, M_DL(M_XMODE) | M_ARG,
     "Firewall   擋信列表"},
 
-    {{.dlfuncarg = {{DL_NAME("adminutil.so", m_resetsys)}, (const void *)6}}, PERM_CHATROOM, M_DL(M_XMODE) | M_ARG,
+    {{DLFUNCARG(DL_NAME("adminutil.so", m_resetsys), 6)}, PERM_CHATROOM, M_DL(M_XMODE) | M_ARG,
     "Xchatd     重開聊天室"},
 
-    {{.dlfuncarg = {{DL_NAME("adminutil.so", m_resetsys)}, (const void *)7}}, PERM_SYSOP, M_DL(M_XMODE) | M_ARG,
+    {{DLFUNCARG(DL_NAME("adminutil.so", m_resetsys), 7)}, PERM_SYSOP, M_DL(M_XMODE) | M_ARG,
     "All        全部"},
 
     {{.menu = menu_admin}, PERM_MENU + 'K', M_ADMIN,
@@ -738,25 +738,25 @@ INTERNAL_INIT MENU menu_talk[] =
 INTERNAL_INIT MENU menu_information[] =
 {
 
-    {{.funcarg = {{menumore}, "gem/@/@pop"}}, 0, M_READA | M_ARG,
+    {{FUNCARG(menumore, "gem/@/@pop")}, 0, M_READA | M_ARG,
     "Login      上站次數排行榜"},
 
-    {{.funcarg = {{menumore}, "gem/@/@-act"}}, 0, M_READA | M_ARG,
+    {{FUNCARG(menumore, "gem/@/@-act")}, 0, M_READA | M_ARG,
     "Today      今日上線人次統計"},
 
-    {{.funcarg = {{menumore}, "gem/@/@=act"}}, 0, M_READA | M_ARG,
+    {{FUNCARG(menumore, "gem/@/@=act")}, 0, M_READA | M_ARG,
     "Yesterday  昨日上線人次統計"},
 
-    {{.funcarg = {{menumore}, "gem/@/@-day"}}, 0, M_READA | M_ARG,
+    {{FUNCARG(menumore, "gem/@/@-day")}, 0, M_READA | M_ARG,
     "0Day       本日十大熱門話題"},
 
-    {{.funcarg = {{menumore}, "gem/@/@-week"}}, 0, M_READA | M_ARG,
+    {{FUNCARG(menumore, "gem/@/@-week")}, 0, M_READA | M_ARG,
     "1Week      本週五十大熱門話題"},
 
-    {{.funcarg = {{menumore}, "gem/@/@-month"}}, 0, M_READA | M_ARG,
+    {{FUNCARG(menumore, "gem/@/@-month")}, 0, M_READA | M_ARG,
     "2Month     本月百大熱門話題"},
 
-    {{.funcarg = {{menumore}, "gem/@/@-year"}}, 0, M_READA | M_ARG,
+    {{FUNCARG(menumore, "gem/@/@-year")}, 0, M_READA | M_ARG,
     "3Year      本年度百大熱門話題"},
 
     {{.menu = menu_xyz}, PERM_MENU + 'L', M_MMENU,
@@ -769,7 +769,7 @@ INTERNAL_INIT MENU menu_xyz[] =
     {{.menu = menu_information}, 0, M_XMENU,
     "Tops       " NICKNAME "排行榜"},
 
-    {{.funcarg = {{menumore}, FN_ETC_VERSION}}, 0, M_READA | M_ARG,
+    {{FUNCARG(menumore, FN_ETC_VERSION)}, 0, M_READA | M_ARG,
     "Version    源碼發展資訊"},
 
     {{.dl = {DL_NAME("xyz.so", x_siteinfo)}}, 0, M_DL(M_READA),
@@ -781,7 +781,7 @@ INTERNAL_INIT MENU menu_xyz[] =
     {{welcome}, 0, M_READA,
     "Welcome    觀看歡迎畫面"},
 
-    {{.funcarg = {{menumore}, FN_ETC_COUNTER}}, 0, M_READA | M_ARG,
+    {{FUNCARG(menumore, FN_ETC_COUNTER)}, 0, M_READA | M_ARG,
     "History    本站歷史軌跡"},
 
     {{.menu = menu_main}, PERM_MENU + 'T', M_SMENU,
@@ -1618,8 +1618,16 @@ domenu_exec(
                 /* Thor.990212: dynamic load, with negative umode */
                 if (mmode & M_DL(0))
                 {
-                    mitem.func = (int (*)(void)) DL_GET(mitem.dl.func);
-                    if (!mitem.func) break;
+                    if (mmode & M_ARG)
+                    {
+                        mitem.funcarg->func = (int (*)(const void *)) DL_GET(mitem.dlfuncarg->func);
+                        if (!mitem.funcarg->func) break;
+                    }
+                    else
+                    {
+                        mitem.func = (int (*)(void)) DL_GET(mitem.dl.func);
+                        if (!mitem.func) break;
+                    }
                     mmode &= ~M_DL(0);
   #ifndef DL_HOTSWAP
                     mptr->item = mitem;
@@ -1641,9 +1649,9 @@ domenu_exec(
                 if (mmode & M_ARG)
                 {
                     if (mmode & M_XO)
-                        res = mitem.funcarg.xofunc(xo, mitem.funcarg.arg);
+                        res = mitem.funcarg->xofunc(xo, mitem.funcarg->arg);
                     else
-                        res = mitem.funcarg.func(mitem.funcarg.arg);
+                        res = mitem.funcarg->func(mitem.funcarg->arg);
                 }
                 else
                 {

maple/popupmenu.c
@@ -66,9 +66,18 @@ do_cmd(MENU *mptr, XO *xo, int y, int x)
 #if !NO_SO
     if (mmode & M_DL(0))
     {
-        mitem.func = (int (*)(void)) DL_GET(mitem.dl.func);
-        if (!mitem.func)
-            return 0;
+        if (mmode & M_ARG)
+        {
+            mitem.funcarg->func = (int (*)(const void *)) DL_GET(mitem.dlfuncarg->func);
+            if (!mitem.funcarg->func)
+                return 0;
+        }
+        else
+        {
+            mitem.func = (int (*)(void)) DL_GET(mitem.dl.func);
+            if (!mitem.func)
+                return 0;
+        }
         mmode &= ~M_DL(0);
   #ifndef DL_HOTSWAP
         mptr->item = mitem;
@@ -88,9 +97,9 @@ do_cmd(MENU *mptr, XO *xo, int y, int x)
     else if (mmode & M_ARG)
     {
         if (mmode & M_XO)
-            mode = mitem.funcarg.xofunc(xo, mitem.funcarg.arg);
+            mode = mitem.funcarg->xofunc(xo, mitem.funcarg->arg);
         else
-            mode = mitem.funcarg.func(mitem.funcarg.arg);
+            mode = mitem.funcarg->func(mitem.funcarg->arg);
     }
     else
     {

include/struct.h
@@ -1088,6 +1088,13 @@ typedef struct {
     const void *arg;
 } DlFuncArg;
 
+/* Helper macros for building `MenuItem` with `DlFuncArg` or `FuncArg` objects */
+
+#define FUNCARG(_func, _arg) \
+    .funcarg = &STATLVAL(FuncArg, {{(_func)}, (const void *)(_arg)})
+#define DLFUNCARG(_func, _arg) \
+    .dlfuncarg = &STATLVAL(DlFuncArg, {{(_func)}, (const void *)(_arg)})
+
 typedef union {
     /* The field to be used is determined by the value of `umode` */
 #if NO_SO
@@ -1107,10 +1114,10 @@ typedef union {
     /* The field to be used is determined by the value of `umode` */
     int (*func) (void);  /* `>= M_FUN` or `M_IDLE` */
     int (*xofunc) (XO *xo);  /* `umode | M_XO` */
-    FuncArg funcarg;  /* `umode | M_ARG` */
+    FuncArg *funcarg;  /* `umode | M_ARG` */
 
     DlMenuItem dl;  /* `M_DL(umode)` */
-    DlFuncArg dlfuncarg;  /* `M_DL(umode) | M_ARG` */
+    DlFuncArg *dlfuncarg;  /* `M_DL(umode) | M_ARG` */
 
     const char *title;  /* `umode | M_MENUTITLE` */
     struct MENU *menu;  /* `>= M_MENU` and `< M_FUN` */

maple/acct.c
@@ -102,6 +102,7 @@ void x_file(int mode,            /* M_XFILES / M_UFILES */
     char buf[ANSILINESIZE];
 
     MENU list[41];
+    FuncArg funcargs[COUNTOF(list)];
 
     int n = 0;
     while ((desc = xlist[n]))
@@ -114,8 +115,8 @@ void x_file(int mode,            /* M_XFILES / M_UFILES */
                 "%*s%s", BMAX(24 - len_strip - 4, 0), "", flist[n] + 4);
                                        /* statue.000703: 註解: +4 去掉目錄 */
         }
-        list[n] =
-            LISTLIT(MENU){{.funcarg = {{x_edit_file}, flist[n]}}, 0, mode | M_ARG, strdup(buf)};
+        funcargs[n] = LISTLIT(FuncArg){{x_edit_file}, flist[n]};
+        list[n] = LISTLIT(MENU){{.funcarg = &funcargs[n]}, 0, mode | M_ARG, strdup(buf)};
         n++;
     }
     sprintf(buf, "%s\n%s", (mode == M_XFILES) ? "編系統檔案" : "編個人檔案",

maple/xover.c
@@ -2251,22 +2251,22 @@ static MENU menu_everyz[] =
     "Favorite 我的最愛"},
 #endif
 
-    {{.funcarg = {{Every_Z_Xover}, (const void *)XZ_GEM}}, 0, POPUP_FUN | POPUP_ARG,
+    {{FUNCARG(Every_Z_Xover, XZ_GEM)}, 0, POPUP_FUN | POPUP_ARG,
     "Gem      精華區"},
 
-    {{.funcarg = {{Every_Z_Xover}, (const void *)XZ_ULIST}}, 0, POPUP_FUN | POPUP_ARG,
+    {{FUNCARG(Every_Z_Xover, XZ_ULIST)}, 0, POPUP_FUN | POPUP_ARG,
     "Ulist    使用者名單"},
 
     {{Every_Z_Board}, 0, POPUP_FUN,
     "Post     文章列表"},
 
-    {{.funcarg = {{Every_Z_Xover}, (const void *)XZ_CLASS}}, 0, POPUP_FUN | POPUP_ARG,
+    {{FUNCARG(Every_Z_Xover, XZ_CLASS)}, 0, POPUP_FUN | POPUP_ARG,
     "Class    看板列表"},
 
     {{Every_Z_MBox}, PERM_BASIC, POPUP_FUN,
     "Mail     信箱"},
 
-    {{.funcarg = {{Every_Z_Xover}, (const void *)XZ_BMW}}, 0, POPUP_FUN | POPUP_ARG,
+    {{FUNCARG(Every_Z_Xover, XZ_BMW)}, 0, POPUP_FUN | POPUP_ARG,
     "Bmw      熱訊紀錄"},
 
     {{Every_Z_Screen}, 0, POPUP_FUN,

commit 369e61326d9aea568248e4130e5d9b845424d7e9

fix(MENU): fix loading of DlFuncArg obj broken since 3acbab2dac434dd2219731b691627d1635f42217
This issue caused crashes when invoking a DlFuncArg menu item
for the 3nd time while hotswapping was enabled
or for the next time after a failed dynamic loading
regardless of whether hotswapping was enabled.

This is because the member `DlFuncArg::func` of the menu item object
was wrongly unconditionally replaced by the result of `DL_GET()`.

- Cases where the 1st loading successed while hotswapping was enabled:

Event    | Accessed field                              | Resulting field
 ---     | ---                                         | ---
Init     | (None)                                      | `DlFuncArg::func` (DL obj name str)
1st load | `DlFuncArg::func` (valid DL obj name str)   | `FuncArg::func` (function pointer)
2nd load | `DlFuncArg::func` (invalid DL obj name str) | `FuncArg::func` (`NULL`)
3rd load | `DlFuncArg::func` (NULL-dereferencing)      | (undefined)

Note that the `MENU::umode` (determines the accessed field) for the menu item
was not updated due to the way hotswapping works.

- Cases where the 1st loading failed, regardless of whether hotswapping was enabled:

Event    | Accessed field                              | Resulting field
 ---     | ---                                         | ---
Init     | (None)                                      | `DlFuncArg::func` (DL obj name str)
1st load | `DlFuncArg::func` (valid; failed to load)   | `FuncArg::func` (`NULL`)
2nd load | `DlFuncArg::func` (NULL-dereferencing)      | (undefined)

This issue affected menu_reset[], the "系統重置" (resetting system) menu.

This issue is fixed by copying the `DlFuncArg` object
to avoid unwanted modifications to the pointed `DlFuncArg` object,
which should not be modified if hotswapping is enabled.

* maple/menu.c: domenu_exec() & maple/popupmenu.c: do_cmd():
    * Move local var `mitem` into the new helper union `MItem m`

maple/menu.c
@@ -1611,26 +1611,40 @@ domenu_exec(
         case '\n':
             {
                 MENU *const mptr = xyz->table[xo->pos];
-                MenuItem mitem = mptr->item;
                 int mmode = mptr->umode;
                 int res;
+                /* IID.2021-12-02: Helper union for proper handling of `M_ARG` */
+                typedef union {
+                    MenuItem item;
+                    FuncArg funcarg;
+                    DlFuncArg dlfuncarg;
+                } MItem;
+                MItem m =
+                    (mmode & M_ARG) ? LISTLIT(MItem){.funcarg = *mptr->item.funcarg} /* Make a copy of the `FuncArg` object */
+                    : LISTLIT(MItem){.item = mptr->item};
 #if !NO_SO
                 /* Thor.990212: dynamic load, with negative umode */
                 if (mmode & M_DL(0))
                 {
                     if (mmode & M_ARG)
                     {
-                        mitem.funcarg->func = (int (*)(const void *)) DL_GET(mitem.dlfuncarg->func);
-                        if (!mitem.funcarg->func) break;
+                        m.funcarg.func = (int (*)(const void *)) DL_GET(m.dlfuncarg.func);
+                        if (!m.funcarg.func) break;
+  #ifndef DL_HOTSWAP
+                        /* Update the `FuncArg` object */
+                        mptr->item.funcarg->func = m.funcarg.func;
+  #endif
                     }
                     else
                     {
-                        mitem.func = (int (*)(void)) DL_GET(mitem.dl.func);
-                        if (!mitem.func) break;
+                        m.item.func = (int (*)(void)) DL_GET(m.item.dl.func);
+                        if (!m.item.func) break;
+  #ifndef DL_HOTSWAP
+                        mptr->item.func = m.item.func;
+  #endif
                     }
                     mmode &= ~M_DL(0);
   #ifndef DL_HOTSWAP
-                    mptr->item = mitem;
                     mptr->umode = mmode;
   #endif
                 }
@@ -1641,7 +1655,7 @@ domenu_exec(
                 {
                     if (xyz->cmdcur_max == 1)
                         xyz->mtail->level = PERM_MENU + mptr->desc[0];
-                    xyz->menu = mitem.menu;
+                    xyz->menu = m.item.menu;
                     cmd = XO_INIT;
                     continue;
                 }
@@ -1649,16 +1663,16 @@ domenu_exec(
                 if (mmode & M_ARG)
                 {
                     if (mmode & M_XO)
-                        res = mitem.funcarg->xofunc(xo, mitem.funcarg->arg);
+                        res = m.funcarg.xofunc(xo, m.funcarg.arg);
                     else
-                        res = mitem.funcarg->func(mitem.funcarg->arg);
+                        res = m.funcarg.func(m.funcarg.arg);
                 }
                 else
                 {
                     if (mmode & M_XO)
-                        res = mitem.xofunc(xo);
+                        res = m.item.xofunc(xo);
                     else
-                        res = mitem.func();
+                        res = m.item.func();
                 }
 
                 utmp_mode(xyz->mtail->umode);

maple/popupmenu.c
@@ -60,27 +60,41 @@ do_cmd(MENU *mptr, XO *xo, int y, int x)
 {
     GCC_UNUSED unsigned int mode;
     screen_backup_t old_screen;
-    MenuItem mitem = mptr->item;
     int mmode = mptr->umode;
+    /* IID.2021-12-02: Helper union for proper handling of `M_ARG` */
+    typedef union {
+        MenuItem item;
+        FuncArg funcarg;
+        DlFuncArg dlfuncarg;
+    } MItem;
+    MItem m =
+        (mmode & M_ARG) ? LISTLIT(MItem){.funcarg = *mptr->item.funcarg} /* Make a copy of the `FuncArg` object */
+        : LISTLIT(MItem){.item = mptr->item};
 
 #if !NO_SO
     if (mmode & M_DL(0))
     {
         if (mmode & M_ARG)
         {
-            mitem.funcarg->func = (int (*)(const void *)) DL_GET(mitem.dlfuncarg->func);
-            if (!mitem.funcarg->func)
+            m.funcarg.func = (int (*)(const void *)) DL_GET(m.dlfuncarg.func);
+            if (!m.funcarg.func)
                 return 0;
+  #ifndef DL_HOTSWAP
+            /* Update the `FuncArg` object */
+            mptr->item.funcarg->func = m.funcarg.func;
+  #endif
         }
         else
         {
-            mitem.func = (int (*)(void)) DL_GET(mitem.dl.func);
-            if (!mitem.func)
+            m.item.func = (int (*)(void)) DL_GET(m.item.dl.func);
+            if (!m.item.func)
                 return 0;
+  #ifndef DL_HOTSWAP
+            mptr->item.func = m.item.func;
+  #endif
         }
         mmode &= ~M_DL(0);
   #ifndef DL_HOTSWAP
-        mptr->item = mitem;
         mptr->umode = mmode;
   #endif
     }
@@ -92,21 +106,21 @@ do_cmd(MENU *mptr, XO *xo, int y, int x)
     {
 //      sprintf(t, "¡i%s¡j", mptr->desc);
         scr_restore_free(&old_screen);
-        return do_menu(mitem.menu, xo, y, x);
+        return do_menu(m.item.menu, xo, y, x);
     }
     else if (mmode & M_ARG)
     {
         if (mmode & M_XO)
-            mode = mitem.funcarg->xofunc(xo, mitem.funcarg->arg);
+            mode = m.funcarg.xofunc(xo, m.funcarg.arg);
         else
-            mode = mitem.funcarg->func(mitem.funcarg->arg);
+            mode = m.funcarg.func(m.funcarg.arg);
     }
     else
     {
         if (mmode & M_XO)
-            mode = mitem.xofunc(xo);
+            mode = m.item.xofunc(xo);
         else
-            mode = mitem.func();
+            mode = m.item.func();
     }
 
     scr_free(&old_screen);