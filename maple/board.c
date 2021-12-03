ccns/dreambbs

commit 15ad16f0bc314e9c354fbdc2ab84de322f145605

Widescreen: Refine UI alignment and make it fill up to 79 characters.

maple/board.c
@@ -1162,14 +1162,14 @@ board_outs(
     str = (!(bits[chn] & BRD_Z_BIT) && brd->blast > brd_visit[chn]) ? "\x1b[1;31m★\x1b[m" : "☆";
 
     char tmp[BTLEN + 1] = {0};
-    int e_cols = (d_cols + 32 > BTLEN) ? BTLEN - 32 : d_cols;
+    int e_cols = (d_cols + 33 > BTLEN) ? BTLEN - 33 : d_cols;
 
     strcpy(tmp, brd->title);
-    if (tmp[e_cols + 31] & 0x80)
+    if (tmp[e_cols + 32] & 0x80)
 //      tmp[e_cols + 33] = '\0';
 //  else
-        tmp[e_cols + 31] = ' ';
-    tmp[e_cols + 32] = '\0';
+        tmp[e_cols + 32] = ' ';
+    tmp[e_cols + 33] = '\0';
 
 /* 081122.cache:看板性質, 不訂閱, 秘密, 好友, 一般 */
     if (bits[chn] & BRD_Z_BIT)
@@ -1231,15 +1231,15 @@ board_outs(
 //      prints("%6d%s%c%-13s\x1b[%sm%-4s \x1b[m%-36s%c %.13s", num, str,
 //      prints("%6d%s%c%-13s\x1b[%sm%-4s \x1b[m%s%c %.13s", num, str,
 
-    prints("%6d%s%c%-13s\x1b[%sm%-4s \x1b[m%-*s %s", num, str, brdtype, brd->brdname, buf, brd->class_, d_cols + 32, tmp, str2);
+    prints("%6d%s%c%-13s\x1b[%sm%-4s \x1b[m%-*s%s", num, str, brdtype, brd->brdname, buf, brd->class_, d_cols + 33, tmp, str2);
 
     strcpy(tmp, brd->BM);
-    if (tmp[13] & 0x80)
-        tmp[12] = '\0';
-    else
+    if (tmp[14] & 0x80)
         tmp[13] = '\0';
+    else
+        tmp[14] = '\0';
 
-    prints("%-13s\n", tmp);
+    prints("%-14s\n", tmp);
 }
 
 void
@@ -1247,7 +1247,7 @@ class_outs(
     const char *title,
     int num)
 {
-    prints("%6d   %s\n", num, title);
+    prints("%6d   %-13.13s    %.*s\n", num, title, d_cols + 53, title + 13);
 }
 
 static void

include/theme.h
@@ -61,7 +61,7 @@
 /* ----------------------------------------------------- */
 
 #define NECKBOARD  "  [←]主選單 [→]閱\讀 [↑↓]選擇 [c]篇數 [y]載入 [/]搜尋 [s]看板 [h]說明\n" \
-            COLOR3 "  %-7s看  板            %-*s人氣 板    主     \x1b[m"
+            COLOR3 "  %-7s看  板            %-*s人氣 板    主      \x1b[m"
 
 /*#define NECKGEM1 \
                    "  [←]離開 [→]瀏覽 [f]模式 [C]暫存 [F]轉寄 [Z]下載 [h]說明 "
@@ -72,22 +72,22 @@
                    /*r2.20170802: disable some tips in case of confusion*/
 
 #define NECKGEM2 \
-            COLOR0 "  編號     主              題%*s                            [編      選] [日  期]\x1b[m"
+            COLOR0 "  編號     主              題%*s                             [編      選] [日  期]\x1b[m"
 
 #define NECKPOST \
                    "  [←]離開 [→]閱\讀 [^P]發表 [b]備忘錄 [d]刪除 [V]投票 [TAB]精華區 [h]說明\n" \
-            COLOR3 "  編號    日 期  作  者       文  章  標  題%*s                                  \x1b[m"
+            COLOR3 "  編號     日 期 作  者       文  章  標  題%*s                                   \x1b[m"
 
 #define NECKMAIL \
                    "  [←]離開 [→]讀信 [d]刪除 [R]回信 [s]寄信 [x]轉貼 [y]群組回信 [h]說明\n" \
-            COLOR0 "  編號    日 期  作 者          信  件  標  題%*s                                \x1b[m"
+            COLOR0 "  編號   日 期 作 者        信  件  標  題%*s                                     \x1b[m"
 
 #define NECKVOTE \
                    "  [←]離開 [v]投票 [R]結果 [E]修改 [^P]舉行 [^Q]查詢/中止/改期 [h]說明\n" \
-            COLOR0 "  編號  開票日  主辦人       投  票  宗  旨%*s                                   \x1b[m"
+            COLOR0 "  編號  開票日  主辦人       投  票  宗  旨%*s                                    \x1b[m"
 
 #define NECKINNBBS "  [←]離開 [^P]新增 [d]刪除 [E]編輯 [/]搜尋 [Enter]詳細\n" \
-            COLOR3 "  編號            內         容%*s                                               \x1b[m"
+            COLOR3 "  編號              內         容%*s                                              \x1b[m"
 
 // IID.190324
 
@@ -95,65 +95,65 @@
 
 #define NECK_BANMAIL \
                    "  [←]離開 ^P)新增 c)修改 d)刪除 S)重整 [h]elp\n" \
-            COLOR3 "  編號  使用率 更新期 模  式  擋  信  內  容%*s                                  \x1b[m"
+            COLOR3 "  編號  使用率 更新期 模  式  擋  信  內  容%*s                                   \x1b[m"
 
 #define NECK_MYFAVORITE \
                    "  [←]主選單 [→]閱\讀 [a]新增 [d]刪除 [c]篇數 [/]搜尋 [s]看板 [h]說明\n" \
-            COLOR0 "  %-7s看  板            %-*s人氣 板    主     \x1b[m"
+            COLOR0 "  %-7s看  板            %-*s人氣 板    主      \x1b[m"
 
 #define NECK_XPOST "\n" /*r2.20181219: this part may need to be refined*/ \
-            COLOR3 "  編號   日 期  作  者       文  章  標  題%*s                                   \x1b[m"
+            COLOR3 "  編號    日 期 作  者       文  章  標  題%*s                                    \x1b[m"
 
 #define NECK_ULIST "\n" \
-            COLOR3 " No.  代號         %-*s%-13s   PM %-14s閒置\x1b[m"
+            COLOR3 "  No.  代號         %-*s%-13s   PM %-14s閒置\x1b[m"
 
 #define NECK_PAL   "  [←]離開 a)新增 c)修改 d)刪除 m)寄信 s)整理 [/?]搜尋 [q]查詢 [h]elp\n" \
-            COLOR3 "  編號    代 號         友       誼%*s                                           \x1b[m"
+            COLOR3 "  編號    代 號         友       誼%*s                                            \x1b[m"
 
 #define NECK_BMW   "  [←]離開  [d]刪除  [m]寄信  [w]快訊  [s]更新  [→]查詢  [h]elp\n" \
-            COLOR3 "  編號 代 號        內       容%*s                                               \x1b[m"
+            COLOR3 "  編號 代 號        內       容%*s                                                \x1b[m"
 
 #define NECK_BMWTIME \
                    "  [←]離開  [d]刪除  [m]寄信  [w]快訊  [s]更新  [→]查詢  [h]elp\n" \
-            COLOR3 "  編號 時 間 代 號        內       容%*s                                         \x1b[m"
+            COLOR3 "  編號 時 間 代 號        內       容%*s                                          \x1b[m"
 
 #define NECK_BANMSG \
                    "  [←]離開 a)新增 c)修改 d)刪除 m)寄信 s)整理 [q]查詢 [h]elp\n" \
-            COLOR3 "  編號    代 號         描       述%*s                                           \x1b[m"
+            COLOR3 "  編號    代 號         描       述%*s                                            \x1b[m"
 
 // so/*
 
 #define NECK_ADMIN \
                    "  [←]離開 ^P)新增 c)修改 d)刪除 s)重整 [h]elp\n" \
-            COLOR3 "  編號     站  務  名  單%*s                                                     \x1b[m"
+            COLOR3 "  編號     站  務  名  單%*s                                                      \x1b[m"
 
 #define NECK_ALOHA \
                    "  [←]離開 a)新增 d)刪除 s)重整 f)引入好友名單 [h]elp\n" \
-            COLOR5 "  編號     上 站 通 知 名 單%*s                                                  \x1b[m"
+            COLOR5 "  編號     上 站 通 知 名 單%*s                                                   \x1b[m"
 
 #define NECK_BRDSTAT \
                    "  [←]離開 s)重整 S)目前資訊 [h]elp\n" \
-            COLOR3 "  編號   型    態  閱\讀次數  發文次數  轉信次數  擋信次數%*s                     \x1b[m"
+            COLOR3 "  編號   型    態  閱\讀次數  發文次數  轉信次數  擋信次數%*s                      \x1b[m"
 
 #define NECK_CHATMENU \
                    "  [←]離開 ^P)新增 c)修改 d)刪除 s)重整 M)搬移 TAB)動詞 f)訊息 [h]elp\n" \
-            COLOR3 "  編號 動詞      中文   %*s                                                \x1b[m"
+            COLOR3 "  編號 動詞      中文   %-*s                                                       \x1b[m"
 
 #define NECK_CLEANRECOMMEND \
                    "  [←]離開 c)修改[站長專用] d)刪除 D)清除全部 s)重整 [h]elp\n" \
-            COLOR0 "  編號 推      使用者 留言%*s                                               日 期\x1b[m"
+            COLOR0 "  編號 推    使用者 留言%*s                                                  日 期\x1b[m"
 
 #define NECK_CONTACT \
                    "  [←]離開 ^P)新增 c)修改 d)刪除 s)重整 m)寄信 [h]elp\n" \
-            COLOR3 "  編號     聯  絡  名  單     e-mail address%*s                                  \x1b[m"
+            COLOR3 "  編號     聯  絡  名  單     e-mail address%*s                                   \x1b[m"
 
 #define NECK_LISTBRD \
                    "  [←]離開 a)新增 d)刪除 s)重整 TAB)切換名單 T)更改群組名稱 /)搜尋\n" \
-            COLOR3 "  編號     看板名稱%*s                                                           \x1b[m"
+            COLOR3 "  編號     看板名稱%*s                                                            \x1b[m"
 
 #define NECK_LISTUSR \
                    "  [←]離開 a)新增 d)刪除 s)重整 TAB)切換名單 T)更改群組名稱 /)搜尋\n" \
-            COLOR3 "  編號     使用者 ID%*s                                                          \x1b[m"
+            COLOR3 "  編號     使用者 ID%*s                                                           \x1b[m"
 
 /*#define NECK_MAILGEM1 \
                    "  [←]離開 [→]瀏覽 [f]模式 [C]暫存 [F]轉寄 [Z]下載 [h]說明 "
@@ -163,37 +163,37 @@
                    /*r2.20170802: disable "Z" option in case of confusion*/
 
 #define NECK_MAILGEM2 \
-            COLOR0 "  編號     主              題%*s                            [編      選] [日  期]\x1b[m"
+            COLOR0 "  編號     主              題%*s                             [編      選] [日  期]\x1b[m"
 
 #define NECK_MEMORANDUM \
                    "  [←]離開 ^P)新增 c)修改 d)刪除 s)重整 [h]elp\n" \
-            COLOR3 "  編號  日期      時間      工作或行程%*s                                        \x1b[m"
+            COLOR3 "  編號  日期      時間      工作或行程%*s                                         \x1b[m"
 
 #define NECK_NBRD  "  [←]離開 [→]閱\讀 [^P]發表 [d]刪除 [j]加入連署 [TAB]精華區 [h]elp\n" \
-            COLOR0 "  編號   日 期 舉辦人        看  版  標  題%*s                                   \x1b[m"
+            COLOR0 "  編號   日 期 舉辦人        看  版  標  題%*s                                    \x1b[m"
 
 #define NECK_OBSERVE \
                    "  [←]離開 ^P)新增 c)修改 d)刪除 S)重整 [h]elp\n" \
-            COLOR3 "  編號 使用者ID      說明%*s                                                     \x1b[m"
+            COLOR3 "  編號 使用者ID      說明%*s                                                      \x1b[m"
 
 #define NECK_PERSONAL1 \
                    "  [←]離開 c)修改 d)刪除 s)重整 TAB)中文版名/E-mail O)開板 D)退件 [h]elp\n" \
             COLOR0 "  編號   申請人       看板名稱     "
 
 #define NECK_PERSONALEMAIL2 \
-                   "E-mail%*s                                  \x1b[m"
+                   "E-mail%*s                                   \x1b[m"
 
 #define NECK_PERSONALTITLE2 \
-                   "中文版名%*s                                \x1b[m"
+                   "中文版名%*s                                 \x1b[m"
 
 #define NECK_SHOW  "  [←]離開 ^P)新增 c)修改 d)刪除 s)重整 [h]elp\n" \
-            COLOR3 "  編號     已投票的 Email%*s                                                     \x1b[m"
+            COLOR3 "  編號     已投票的 Email%*s                                                      \x1b[m"
 
 #define NECK_VIOL  "  [←]離開 ^P)新增 c)修改 d)刪除 f)搜尋 [h]elp\n" \
-            COLOR3 "  編號 次數 時間 禁止註冊 Email%*s                                               \x1b[m"
+            COLOR3 "  編號 次數 時間 禁止註冊 Email%*s                                                \x1b[m"
 
 #define NECK_SONG  "  [←]離開 [→]瀏覽 [o]點歌到動態看板 [m]點歌到信箱 [q]查詢剩餘次數 [h]說明 \n" \
-            COLOR0 "  編號     主              題%*s                            [編      選] [日  期]\x1b[m"
+            COLOR0 "  編號     主              題%*s                             [編      選] [日  期]\x1b[m"
 
 // cache.101119
 
@@ -207,24 +207,24 @@
 /* ----------------------------------------------------- */
 
 
-/* itoc.010914.註解: 單一篇，所以叫 FOOTER，都是 78 char */
+/* itoc.010914.註解: 單一篇，所以叫 FOOTER，都是 79 char */
 
 #define FOOTER_POST     \
-    COLOR1 " 文章選讀 " COLOR2 " (ry)回應 (=\\[]<>-+;'`)主題 (|?QA)搜尋標題作者 (kj)上下篇 (C)暫存   "
+    COLOR1 " 文章選讀 " COLOR2 " (ry)回應 (=\\[]<>-+;'`)主題 (|?QA)搜尋標題作者 (kj)上下篇 (C)暫存    "
 
 #define FOOTER_MORE     \
-    COLOR1 " 瀏覽 P.%d (%d%%) " COLOR2 " (h)說明 [PgUp][PgDn][0][$]移動 (/n)搜尋 (C)暫存 (←q)結束 "
+    COLOR1 " 瀏覽 P.%d (%d%%) " COLOR2 " (h)說明 [PgUp][PgDn][0][$]移動 (/n)搜尋 (C)暫存 (←q)結束  "
 
 
 #define FOOTER_TALK \
-    COLOR1 " 交談模式 " COLOR2 " (^A)\x1b[30m對奕模式 \x1b[31m(^B)\x1b[30m象棋模式 \x1b[31m(^C, ^D)\x1b[30m結束交談 \x1b[31m(^Z)\x1b[30m快捷列表 \x1b[31m(^G)\x1b[30m嗶嗶   "
+    COLOR1 " 交談模式 " COLOR2 " (^A)\x1b[30m對奕模式 \x1b[31m(^B)\x1b[30m象棋模式 \x1b[31m(^C, ^D)\x1b[30m結束交談 \x1b[31m(^Z)\x1b[30m快捷列表 \x1b[31m(^G)\x1b[30m嗶嗶    "
 
 
 #define FOOTER_VEDIT_BIFF \
-               "%s\x1b[m" COLOR2 "  (Ctrl-Z)\x1b[30m 操作說明  \x1b[31m(^w, ^x)\x1b[30m 檔案處理 ║%s│%s║%5d:%3d%*s    \x1b[m"
+               "%s\x1b[m" COLOR2 "  (Ctrl-Z)\x1b[30m 操作說明  \x1b[31m(^w, ^x)\x1b[30m 檔案處理 ║%s│%s║%5d:%3d%*s     \x1b[m"
 
 #define FOOTER_VEDIT \
-    COLOR1 "  編輯文章  " COLOR2 "  (Ctrl-Z)\x1b[30m 操作說明  \x1b[31m(^w, ^x)\x1b[30m 檔案處理 ║%s│%s║%5d:%3d%*s    \x1b[m"
+    COLOR1 "  編輯文章  " COLOR2 "  (Ctrl-Z)\x1b[30m 操作說明  \x1b[31m(^w, ^x)\x1b[30m 檔案處理 ║%s│%s║%5d:%3d%*s     \x1b[m"
 
 
 /*#define FOOTER_BMW_REPLY \
@@ -235,13 +235,13 @@
 
 
 #define MSG_POST \
-    COLOR1 " 文章選讀 " COLOR2 " (y)\x1b[30m回應 \x1b[31m(=\\[]<>-+;'`jk)\x1b[30m相關主題 \x1b[31m(/?)\x1b[30m搜尋標題 \x1b[31m(aA)\x1b[30m搜尋作者          "
+    COLOR1 " 文章選讀 " COLOR2 " (y)\x1b[30m回應 \x1b[31m(=\\[]<>-+;'`jk)\x1b[30m相關主題 \x1b[31m(/?)\x1b[30m搜尋標題 \x1b[31m(aA)\x1b[30m搜尋作者           "
 
 #define MSG_MAILER \
-    COLOR1 " 魚雁往返 " COLOR2 " (r)\x1b[30m回信 \x1b[31m(x)\x1b[30m轉達 \x1b[31m(y)\x1b[30m群組回信 \x1b[31m(d)\x1b[30m刪除 \x1b[31m[m]\x1b[30m標記                        "
+    COLOR1 " 魚雁往返 " COLOR2 " (r)\x1b[30m回信 \x1b[31m(x)\x1b[30m轉達 \x1b[31m(y)\x1b[30m群組回信 \x1b[31m(d)\x1b[30m刪除 \x1b[31m[m]\x1b[30m標記                         "
 
 #define MSG_GEM \
-    COLOR1 " 閱\讀精華 " COLOR2 " (=\\[]<>-+jk)\x1b[30m相關主題 \x1b[31m(/?aA)\x1b[30m搜尋標題作者 \x1b[31m(↑↓)\x1b[30m上下 \x1b[31m(←)\x1b[30m離開        "
+    COLOR1 " 閱\讀精華 " COLOR2 " (=\\[]<>-+jk)\x1b[30m相關主題 \x1b[31m(/?aA)\x1b[30m搜尋標題作者 \x1b[31m(↑↓)\x1b[30m上下 \x1b[31m(←)\x1b[30m離開         "
 
 
 /* ----------------------------------------------------- */
@@ -249,13 +249,13 @@
 /* ----------------------------------------------------- */
 
 
-/* itoc.010914.註解: 列表多篇，所以叫 FEETER，都是 78 char */
+/* itoc.010914.註解: 列表多篇，所以叫 FEETER，都是 79 char */
 
 #define FEETER_INNBBS   \
-    COLOR1 " 轉信設定 " COLOR2 " (↑/↓)上下 (PgUp/PgDn)上下頁 (Home/End)首尾 (←)(q)離開           "
+    COLOR1 " 轉信設定 " COLOR2 " (↑/↓)上下 (PgUp/PgDn)上下頁 (Home/End)首尾 (←)(q)離開            "
 
 #define FEETER_VOTEALL  \
-    COLOR1 " 投票中心 " COLOR2 " (↑/↓)上下 (PgUp/PgDn)上下頁 (Home/End)首尾 (→)投票 (←)(q)離開  "
+    COLOR1 " 投票中心 " COLOR2 " (↑/↓)上下 (PgUp/PgDn)上下頁 (Home/End)首尾 (→)投票 (←)(q)離開   "
 
 /* ----------------------------------------------------- */
 /* 站簽：站台來源簽名                                    */

maple/banmail.c
@@ -34,7 +34,7 @@ static void banmail_item(int num, const BANMAIL * ban)
 
     now = ((ban->time - time(0) + BANMAIL_EXPIRE * 86400) / 3600);
     prints("%6d  %6d %6ld %s  %-*.*s\n", num, ban->usage, BMAX(now, (time_t)0),
-           modes, d_cols + 48, d_cols + 48, ban->data);
+           modes, d_cols + 49, d_cols + 49, ban->data);
 }
 
 static int banmail_body(XO * xo)

maple/gem.c
@@ -111,10 +111,10 @@ gem_item(
         if (xmode & GEM_BOARD)
         {
             sprintf(fpath, "gem/brd/%s/", ghdr->xname);
-            prints("%-*.*s%-13s%s\n", d_cols + 46, d_cols + 45, ghdr->title, (gtype == 1 ? ghdr->xname : ghdr->owner), access(fpath, R_OK) ? "[deleted]" : ghdr->date);
+            prints("%-*.*s%-13s%s\n", d_cols + 47, d_cols + 46, ghdr->title, (gtype == 1 ? ghdr->xname : ghdr->owner), access(fpath, R_OK) ? "[deleted]" : ghdr->date);
         }
         else
-            prints("%-*.*s%-13s%s\n", d_cols + 46, d_cols + 45, ghdr->title, (gtype == 1 ? ghdr->xname : ghdr->owner), ghdr->date);
+            prints("%-*.*s%-13s%s\n", d_cols + 47, d_cols + 46, ghdr->title, (gtype == 1 ? ghdr->xname : ghdr->owner), ghdr->date);
     }
 }
 

maple/mail.c
@@ -2154,9 +2154,9 @@ mbox_item(
 #endif
 
     int xmode = hdr->xmode;
-    prints(xmode & MAIL_DELETE ? "%5d \x1b[1;5;37;41m%c\x1b[m"
-        : xmode & MAIL_MARKED ? "%5d \x1b[1;36m%c\x1b[m"
-        : "%5d %c", pos, mbox_attr(hdr->xmode));
+    prints(xmode & MAIL_DELETE ? "%6d \x1b[1;5;37;41m%c\x1b[m"
+        : xmode & MAIL_MARKED ? "%6d \x1b[1;36m%c\x1b[m"
+        : "%6d %c", pos, mbox_attr(hdr->xmode));
 
     hdr_outs(hdr, d_cols + 47);
 }

maple/menu.c
@@ -331,8 +331,8 @@ void
 vs_head(
     const char *title, const char *mid)
 {
-    char buf[(T_COLS - 1) - 79 + 69 + 1];     /* d_cols 最大可能是 (T_COLS - 1) */
-    char ttl[(T_COLS - 1) - 79 + 69 + 1];
+    char buf[(T_COLS - 1) - 79 + 66 + 29 + 1];     /* d_cols 最大可能是 (T_COLS - 1) */
+    char ttl[(T_COLS - 1) - 79 + 66 + 29 + 1];
     int spc, len;
     unsigned int ufo;
 #ifdef  COLOR_HEADER
@@ -354,7 +354,7 @@ vs_head(
     spc = strlen(mid);
     ufo = cutmp->ufo;
 
-    len = d_cols + 65 - strlen(title) - strlen(currboard); /* len: 中間還剩下多長的空間 */
+    len = d_cols + 66 - strlen(title) - strlen(currboard); /* len: 中間還剩下多長的空間 */
     if (ufo & UFO_BIFF)
     {
         mid = NEWMAILMSG; // 你有新情書

maple/more.c
@@ -302,13 +302,13 @@ outs_header(    /* 印出檔頭 */
         {
             ptr[-1] = ptr[4] = '\0';
             prints(COLOR5 " %s " COLOR6 "%-*.*s" COLOR5 " %s " COLOR6 "%-13s\x1b[m",
-                header1[0], d_cols + 53, d_cols + 53, word, ptr, ptr + 5);
+                header1[0], d_cols + 54, d_cols + 54, word, ptr, ptr + 5);
         }
         else
         {
             /* 少看板這欄 */
             prints(COLOR5 " %s " COLOR6 "%-*.*s\x1b[m",
-                header1[0], d_cols + 72, d_cols + 72, word);
+                header1[0], d_cols + 73, d_cols + 73, word);
         }
         return;
     }
@@ -321,7 +321,7 @@ outs_header(    /* 印出檔頭 */
             /* 其他檔頭都只有一欄 */
             word = str + header_len;
             prints(COLOR5 " %s " COLOR6 "%-*.*s\x1b[m",
-                header1[i], d_cols + 72, d_cols + 72, word);
+                header1[i], d_cols + 73, d_cols + 73, word);
             return;
         }
     }
@@ -450,7 +450,8 @@ more(
     fend = fimage + fsize;
 
     /* more_width = b_cols - 1; */      /* itoc.070517.註解: 若用這個，每列最大字數會和 header 及 footer 對齊 (即會有留白二格) */
-    more_width = b_cols + 1;            /* itoc.070517.註解: 若用這個，每列最大字數與螢幕同寬 */
+    /* more_width = b_cols + 1; */      /* itoc.070517.註解: 若用這個，每列最大字數與螢幕同寬 */
+    more_width = b_cols;  /* IID.20191223: Leave one space */
 
     /* 找檔頭結束的地方 */
     for (i = 0; i < LINE_HEADER; i++)

maple/talk.c
@@ -1032,16 +1032,16 @@ bmw_item(
             if (!(*bmw->userid))
                 strcpy(bmw->userid, "眾家好友");
 
-            prints("%5d %02d:%02d %-13s☆%-*.*s\n", num, ptime->tm_hour, ptime->tm_min,
+            prints("%6d %02d:%02d %-13s☆%-*.*s\n", num, ptime->tm_hour, ptime->tm_min,
                 bmw->userid, d_cols + 50, d_cols + 50, bmw->msg);
         }
         else
         {
             if (strstr(bmw->msg, "★廣播"))
-                prints("%5d \x1b[36;1m%02d:%02d %-13s★%-*.*s\x1b[m\n", num, ptime->tm_hour, ptime->tm_min,
+                prints("%6d \x1b[36;1m%02d:%02d %-13s★%-*.*s\x1b[m\n", num, ptime->tm_hour, ptime->tm_min,
                     bmw->userid, d_cols + 50, d_cols + 50, (bmw->msg)+8);
             else
-                prints("%5d \x1b[32m%02d:%02d %-13s★%-*.*s\x1b[m\n", num, ptime->tm_hour, ptime->tm_min,
+                prints("%6d \x1b[32m%02d:%02d %-13s★%-*.*s\x1b[m\n", num, ptime->tm_hour, ptime->tm_min,
                     bmw->userid, d_cols + 50, d_cols + 50, bmw->msg);
         }
     }
@@ -1052,15 +1052,15 @@ bmw_item(
             if (!(*bmw->userid))
                 strcpy(bmw->userid, "眾家好友");
 
-            prints("%5d %-13s☆%-*.*s\n", num, bmw->userid, d_cols + 57, d_cols + 57, bmw->msg);
+            prints("%6d %-13s☆%-*.*s\n", num, bmw->userid, d_cols + 57, d_cols + 57, bmw->msg);
         }
         else
         {
             if (strstr(bmw->msg, "★廣播"))
-                prints("%5d \x1b[36;1m%-13s★%-*.*s\x1b[m\n", num,
+                prints("%6d \x1b[36;1m%-13s★%-*.*s\x1b[m\n", num,
                     bmw->userid, d_cols + 57, d_cols + 57, (bmw->msg)+8);
             else
-                prints("%5d \x1b[32m%-13s★%-*.*s\x1b[m\n", num,
+                prints("%6d \x1b[32m%-13s★%-*.*s\x1b[m\n", num,
                     bmw->userid, d_cols + 57, d_cols + 57, bmw->msg);
         }
     }
@@ -2548,7 +2548,7 @@ talk_speak(
 
     utmp_mode(M_TALK);
 
-    ch = 59 + d_cols - strlen(page_requestor);
+    ch = 60 + d_cols - strlen(page_requestor);
 
     sprintf(buf, "%s【%s", cuser.userid, cuser.username);
 
@@ -3160,7 +3160,7 @@ ulist_body(
 
                 strcpy(color, wcolor[fcolor]);
 
-                prints("%5d%c%s%-13s%-*.*s%s%-16.15s%c%c %-12.12s %5.5s",
+                prints("%6d%c%s%-13s%-*.*s%s%-16.15s%c%c %-12.12s %5.5s",
                     cnt, (up->ufo & UFO_WEB)?'*':' ',
                     color, up->userid,
                     d_cols + 22, d_cols + 21, (HAS_PERM(PERM_SYSOP) && (cuser.ufo2 & UFO2_REALNAME))? up->realname : up->username,

commit 2bd076be2b5f72efa1d600183be0055e9a845f17

fix(class): fix category text broken since 15ad16f0bc314e9c354fbdc2ab84de322f145605
Also move the description 1 char to the right
to be consistent with the display of boards.

Format: <ClassID '/'>(IDLEN) (<pad> (5) | <pad>(1) Category(4)) <pad>(1) Description

* class_outs()

maple/board.c
@@ -1218,7 +1218,10 @@ class_outs(
     const char *title,
     int num)
 {
-    prints("%6d   %-*.*s     %.*s\n", num, IDLEN, IDLEN, title, d_cols + 53, title + IDLEN + 1);
+    /* IID.2021-12-04: Format: <ClassID '/'>(IDLEN) (<pad> (5) | <pad>(1) Category(4)) <pad>(1) Description */
+    const bool has_cate = (title[IDLEN] != ' ' && title[IDLEN] != '/');
+    const char *const desc = title + IDLEN + (has_cate ? 4 : 0);
+    prints("%6d   %-.*s %*.*s %.*s\n", num, IDLEN, title, 4, 4, has_cate ? title + IDLEN : "", d_cols + 52, desc + strspn(desc, " "));
 }
 
 static int