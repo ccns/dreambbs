ccns/dreambbs

commit 3e8d7e129ae8ecebe943eee37374975f87e2f5c3

perf(xchatd): refactor out 2 tmp bufs about ID
* chat_ignore(): userid[]
* command_execute(): chatid[]

maple/xchatd.c
@@ -2039,14 +2039,12 @@ chat_ignore(
             if ((list = cu->ignore))
             {
                 int len;
-                char userid[16];
 
                 send_to_user(cu, "◆ 這些人被打入冷宮了：", 0, MSG_MESSAGE);
                 len = 0;
                 do
                 {
-                    sprintf(userid, "%-*s ", IDLEN, list->userid);
-                    strcpy(buf + len, userid);
+                    sprintf(buf + len, "%-*s ", IDLEN, list->userid);
                     len += IDLEN + 1;
                     if (len >= 78)
                     {
@@ -2767,10 +2765,7 @@ command_execute(
         {
             if (cu->room && !CLOAK(cu)) /* 隱身的人也不能說話哦 */
             {
-                char chatid[16];
-
-                sprintf(chatid, "%s:", cu->chatid);
-                sprintf(buf, "%-10s%s", chatid, msg);
+                sprintf(buf, "%-10s:%s", cu->chatid, msg);
                 send_to_room(cu->room, buf, cu->userno, MSG_MESSAGE);
             }
         }

commit a505120276531e2b8153758f13346daac99719d2

fix(xchatd): fix wrong chat msg format caused by 3e8d7e129ae8ecebe943eee37374975f87e2f5c3
This makes the chat msg appear the same as before the problematic commit
for chatid with length <= 8 (which does always hold for the current code).

maple/xchatd.c
@@ -2765,7 +2765,7 @@ command_execute(
         {
             if (cu->room && !CLOAK(cu)) /* 隱身的人也不能說話哦 */
             {
-                sprintf(buf, "%-10s:%s", cu->chatid, msg);
+                sprintf(buf, "%-s:%*s %s", cu->chatid, BMAX(0, 10 - (int)(unsigned)strlen(cu->chatid) - 1), "", msg);
                 send_to_room(cu->room, buf, cu->userno, MSG_MESSAGE);
             }
         }