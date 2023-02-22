ccns/dreambbs

commit 8d94c154c34b68fa73bbc1e791b9ea3da73c525d

refactor(maple/mail.c bsmtp()): eliminate duplicated smtp handling code using macro SMTP_TRY_SENDF()
* Add macro SMTP_TRY_SENDF() for sending a formatted message
  and receiving mail server responds

maple/mail.c
@@ -331,43 +331,25 @@ bsmtp(
         while (buf[3] == '-') /* maniac.bbs@WMStar.twbbs.org 2000.04.18 */
             fgets(buf, sizeof(buf), fr);
 
+#define SMTP_TRY_SENDF(...) do { \
+    fprintf(fw, __VA_ARGS__); \
+    fflush(fw); \
+    do \
+    { \
+        fgets(buf, sizeof(buf), fr); \
+        if (memcmp(buf, "250", 3)) \
+            goto smtp_error; \
+    } while (buf[3] == '-'); \
+} while (0)
+
         /* Thor.990125: MYHOSTNAME統一放入 str_host */
-        fprintf(fw, "HELO %s\r\n", str_host);
-        fflush(fw);
-        do
-        {
-            fgets(buf, sizeof(buf), fr);
-            if (memcmp(buf, "250", 3))
-                goto smtp_error;
-        } while (buf[3] == '-');
+        SMTP_TRY_SENDF("HELO %s\r\n", str_host);
+        SMTP_TRY_SENDF("MAIL FROM:<%s>\r\n", from);
+        SMTP_TRY_SENDF("RCPT TO:<%s>\r\n", rcpt);
+/*      SMTP_TRY_SENDF("DATA\r\n", rcpt);*/ /* statue.000713 */
+        SMTP_TRY_SENDF("DATA\r\n");
 
-        fprintf(fw, "MAIL FROM:<%s>\r\n", from);
-        fflush(fw);
-        do
-        {
-            fgets(buf, sizeof(buf), fr);
-            if (memcmp(buf, "250", 3))
-                goto smtp_error;
-        } while (buf[3] == '-');
-
-        fprintf(fw, "RCPT TO:<%s>\r\n", rcpt);
-        fflush(fw);
-        do
-        {
-            fgets(buf, sizeof(buf), fr);
-            if (memcmp(buf, "250", 3))
-                goto smtp_error;
-        } while (buf[3] == '-');
-
-/*      fprintf(fw, "DATA\r\n", rcpt);*/ /* statue.000713 */
-        fprintf(fw, "DATA\r\n");
-        fflush(fw);
-        do
-        {
-            fgets(buf, sizeof(buf), fr);
-            if (memcmp(buf, "354", 3))
-                goto smtp_error;
-        } while (buf[3] == '-');
+#undef SMTP_TRY_SENDF
 
         /* ------------------------------------------------- */
         /* begin of mail header                              */

commit dae07f76efb4a03aea05803b97efe782b0513679

fix(mail bsmtp()): fix SMTP cmd `DATA` failing due to SMTP_TRY_SENDF() expected status 250, introduced in 8d94c154c34b68fa73bbc1e791b9ea3da73c525d
The expected status code for `DATA` is 354.

* add parameter `expect` to macro SMTP_TRY_SENDF()
* specify the argument `expect` to be 354 for `DATA`

maple/mail.c
@@ -331,23 +331,23 @@ bsmtp(
         while (buf[3] == '-') /* maniac.bbs@WMStar.twbbs.org 2000.04.18 */
             fgets(buf, sizeof(buf), fr);
 
-#define SMTP_TRY_SENDF(...) do { \
+#define SMTP_TRY_SENDF(expected, ...) do { \
     fprintf(fw, __VA_ARGS__); \
     fflush(fw); \
     do \
     { \
         fgets(buf, sizeof(buf), fr); \
-        if (strncmp(buf, "250", 3)) \
+        if (strncmp(buf, CPP_STR(expected), 3)) \
             goto smtp_error; \
     } while (buf[3] == '-'); \
 } while (0)
 
         /* Thor.990125: MYHOSTNAME統一放入 str_host */
-        SMTP_TRY_SENDF("HELO %s\r\n", str_host);
-        SMTP_TRY_SENDF("MAIL FROM:<%s>\r\n", from);
-        SMTP_TRY_SENDF("RCPT TO:<%s>\r\n", rcpt);
-/*      SMTP_TRY_SENDF("DATA\r\n", rcpt);*/ /* statue.000713 */
-        SMTP_TRY_SENDF("DATA\r\n");
+        SMTP_TRY_SENDF(250, "HELO %s\r\n", str_host);
+        SMTP_TRY_SENDF(250, "MAIL FROM:<%s>\r\n", from);
+        SMTP_TRY_SENDF(250, "RCPT TO:<%s>\r\n", rcpt);
+/*      SMTP_TRY_SENDF(354, "DATA\r\n", rcpt);*/ /* statue.000713 */
+        SMTP_TRY_SENDF(354, "DATA\r\n");
 
 #undef SMTP_TRY_SENDF
 