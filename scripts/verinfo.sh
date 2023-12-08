ccns/dreambbs

commit 538da2caac923b2c6279600df0bb399e13f148f0

Verinfo: Add 'scripts/verinfo.sh' to generate git version info. [ref: ptt/pttbbs]
- Add 'scripts/verinfo.sh' to generate
   git version and build configuration information
- CMakeLists.txt & Makefile:
   - Add target `verinfo`
   - Make the source file include the output file `verinfo_export.conf`
      with flag `-imacros` when compiling
- Makefile: Add dependency `verinfo` to `all` and `njob`.
- CMakeLists.txt: Add dependency `verinfo` to all targets.
- .gitignore: Ignore the output file 'verinfo_export.conf'.

scripts/verinfo.sh
@@ -1,0 +1,73 @@
+#!/usr/bin/env bash
+
+# prevent localized logs
+LC_ALL=C
+export LC_ALL
+
+source_dir="${source_dir:-"."}"
+build_make="$1"
+build_arch="$2"
+
+format_url() {
+    echo "$@" | sed 's/^https\?:\/\///g' | sed 's/\.git$//g'
+}
+
+format_commit() {
+    echo "c$(git rev-list --count "$1" 2>/dev/null)/$(git rev-parse --short "$1" 2>/dev/null)"
+}
+
+build_branch="$(git symbolic-ref --short HEAD 2>/dev/null)"
+# if [ -z "${build_branch}" ]; then
+#     build_branch=""
+# fi
+
+build_head="$(format_commit HEAD)"
+if ! git diff --quiet 2>/dev/null; then
+  build_head="${build_head} Modified"
+fi
+
+# `branch.<name>.pushRemote` > `remote.pushDefault` > `branch.<name>.remote`
+build_remote="$(git config --get "branch.${build_branch}.pushRemote" 2>/dev/null)"
+if [ -z "${build_remote}" ]; then
+    build_remote="$(git config --get "remote.pushDefault" 2>/dev/null)"
+fi
+if [ -z "${build_remote}" ]; then
+    build_remote="$(git config --get "branch.${build_branch}.remote" 2>/dev/null)"
+fi
+# `<remote>/<branch.<name>.merge>` > `<remote>/<branch>` > `<remote>/master` > `origin/master`
+build_branch_remote="${build_remote}/$(git config --get "branch.${build_branch}.merge" 2>/dev/null | sed 's/^.*\///g')"
+if ! git rev-parse -q --verify "${build_branch_remote}" >/dev/null; then
+    build_branch_remote="${build_remote}/${build_branch}"
+fi
+if ! git rev-parse -q --verify "${build_branch_remote}" >/dev/null; then
+    build_branch_remote="${build_remote}/master"
+fi
+if ! git rev-parse -q --verify "${build_branch_remote}" >/dev/null; then
+    build_branch_remote="origin/master"
+fi
+build_head_remote="$(format_commit ${build_branch_remote})"
+build_remote="${build_branch_remote%/*}"
+build_branch_remote="${build_branch_remote##*/}"
+
+build_remote_url="$(format_url "$(git config --get "remote.${build_remote}.url" 2>/dev/null)")"
+
+build_date="$(date)"
+build_time="$(date -d"${build_date}" "+%s")"
+
+export_file="${source_dir}/verinfo_export.conf"
+
+printf "\033[1;36mGenerating '%s'...\033[0m\n" "${export_file}"
+cat > "${export_file}" <<EOF
+#define BUILD_MAKE "${build_make}"
+#define BUILD_ARCH "${build_arch}"
+#define BUILD_BRANCH "${build_branch}"
+#define BUILD_HEAD "${build_head}"
+#define BUILD_BRANCH_REMOTE "${build_branch_remote}"
+#define BUILD_HEAD_REMOTE "${build_head_remote}"
+#define BUILD_REMOTE_URL "${build_remote_url}"
+#define BUILD_DATE "${build_date}"
+#define BUILD_TIME ${build_time}L
+EOF
+printf "\033[1;33m"
+cat "${export_file}"
+printf "\033[0m"

.gitignore
@@ -35,6 +35,9 @@
 make_export.conf
 maple/make_export.conf
 
+# git version info
+verinfo_export.conf
+
 # add split-out build directory for cmake
 build/
 

CMakeLists.txt
@@ -107,6 +107,7 @@ set(BBSCONF "${PROJECT_SOURCE_DIR}/dreambbs.conf")
 set(BBSCONF_ORIGIN "${PROJECT_SOURCE_DIR}/include/config.h")
 set(EXPORT_PROJECT "${PROJECT_SOURCE_DIR}/make_export.conf")
 set(EXPORT_MAPLE "${PROJECT_SOURCE_DIR}/maple/make_export.conf")
+set(EXPORT_VERINFO "${PROJECT_SOURCE_DIR}/verinfo_export.conf")
 
 if(NOT EXISTS ${EXPORT_PROJECT})
     execute_process(COMMAND touch ${EXPORT_PROJECT})
@@ -260,6 +261,13 @@ function(EXPORT_CONF export_file)
 endfunction()
 
 EXPORT_CONF(${CMAKE_CURRENT_SOURCE_DIR}/make_export.conf)
+
+add_custom_target(verinfo
+    ALL
+    sh scripts/verinfo.sh CMake ${MULTIARCH}
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+)
+add_compile_options(-imacros "${EXPORT_VERINFO}")
 
 set(CMAKE_INSTALL_PREFIX ${BBSHOME})
 

Makefile
@@ -20,7 +20,7 @@ EXPORT_FILE	:= "$(REALSRCROOT)/make_export.conf"
 # some directories need to be compiled:
 # lib innbbsd maple so util test
 
-all:
+all: verinfo
 	@(cd lib; $(MAKE) all)
 	@(cd maple; $(MAKE) all)
 	@(cd util; $(MAKE) all)
@@ -30,7 +30,7 @@ all:
 .endif
 	@(cd test; $(MAKE) all)
 
-njob:
+njob: verinfo
 	@(cd lib; $(MAKE) -j$(NPROC) all)
 	@(cd maple; $(MAKE) -j$(NPROC) all)
 	@(cd util; $(MAKE) -j$(NPROC) all)
@@ -62,6 +62,9 @@ configure:
 	@$(TARGETS_REST::=$(.TARGETS:tW:C/^ *configure *//))
 	# Continue execution with a new `bmake` instace and stop current `bmake` instance
 	@if [ "$(TARGETS_REST)" ]; then sh -c "$(MAKE) $(TARGETS_REST) $(MAKEFLAGS)"; printf "\033[1;36mJob done. Force stop.\033[m\n" >&2; false; fi
+
+verinfo: .PHONY
+	@sh scripts/verinfo.sh "BSD-make" "$(MULTIARCH)"
 
 runtest:
 	@(cd test; $(MAKE) runtest)

dreambbs.mk
@@ -50,7 +50,9 @@ EXPORTCONF = echo $(DEFCONF$(exconf::= $(exconf:M*))$(exvalue::= $(exvalue:M*)))
 BBSCONF		:= $(REALSRCROOT)/dreambbs.conf
 BBSCONF_ORIGIN		:= $(REALSRCROOT)/include/config.h
 EXPORT_MAPLE	:= $(REALSRCROOT)/maple/make_export.conf
+EXPORT_VERINFO	:= $(REALSRCROOT)/verinfo_export.conf
 != touch $(EXPORT_MAPLE)
+CFLAGS_MK	+= -imacros "$(EXPORT_VERINFO)"
 
 # User names and group names
 BBSUSR != $(GETVAR$(var::= "$(BBSUSR)")$(else_var::= $(GETVALUE$(conf::= "BBSUSR")$(default::= "$(:!id -un!)")$(hdr::= $(BBSCONF_ORIGIN)))))

commit 02ec1f55279f4336a778f65b545dc72ce44de837

build(verinfo.sh): prevent "verinfo_export.conf" from making `make` rebuild by modifying it "in 1970"
This issue was introduced in 538da2caac923b2c6279600df0bb399e13f148f0, where scripts/verinfo.sh has been added.

scripts/verinfo.sh
@@ -68,6 +68,7 @@ iconv -t big5 -o "${export_file}" <<EOF
 #define BUILD_DATE "${build_date}"
 #define BUILD_TIME ${build_time}L
 EOF
+touch -d "@0" "${export_file}"
 printf "\033[1;33m"
 iconv -f big5 "${export_file}"
 printf "\033[0m"